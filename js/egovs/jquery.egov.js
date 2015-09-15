e2a39f3f8a7de76e334543e5cd8f98ae8b8c70c6
Rene Mütterlein <r.muetterlein@b3-it.de>
2015-09-15 11:46:49 +0200

    *ALLE :: Basis-Layout für Sachsen (Klick-Navigation mit einenem NameSpace)

---
+++ js/egovs/jquery.egov.js
@@ -0,0 +1,19 @@
+﻿var eGovMenu = {
+    'init': function(element) {
+        $j(element + ' li>a').click(function(e) {
+            e.preventDefault();
+            if( $j(this).parent().find('ul').length > 0 ) {
+                eGovMenu.toggleMenu( $j(this).next() );
+            }
+            else {
+                eGovMenu.openURL( $j(this).attr('href') );
+            }
+        });
+    },
+    'toggleMenu': function(target) {
+        target.toggle();
+    },
+    'openURL': function(url) {
+        window.location.href = url;
+    }
+};
