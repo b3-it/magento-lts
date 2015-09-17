5052e6f48c0caedea4a67560b95b85426990120f
Rene Mütterlein <r.muetterlein@b3-it.de>
2015-09-17 15:49:22 +0200

    *ALLE :: Basis-Layout für Sachsen

---
+++ js/egovs/jquery.prevent.lostsession.js
@@ -0,0 +1,25 @@
+﻿$j(document).ready(function(){
+    // Loading-Layer beim Klicken auf Link sichtbar machen
+    $j('a').click(function(){
+        if ( $j(this).next().size() > 0 ) {
+            // Abfrage sichert die Klick-Navigation(en) gegen den Layer ab
+            if ( ($j(this).parent().get(0).tagName != 'LI') &&
+                 ($j(this).next().get(0).tagName != 'UL') &&
+                 ($j(this).next().get(0).id != 'header-cart') &&     // Link des Header-Warenkorb
+                 (!$j(this)[0].hasAttribute('data-target-element'))  // Link zum Benutzer-Menü
+               ) {
+                $j('#loading-mask').toggle();
+            }
+        }
+        else {
+            if ( !$j(this).hasClass('skip-link-close') ) {     // Schließen-Button im Header-Warenkorb
+                $j('#loading-mask').toggle();
+            }
+        }
+    });
+
+    // Loading-Layer beim Button auf Link sichtbar machen
+    $j('button').click(function(){
+        $j('#loading-mask').toggle();
+    });
+});
