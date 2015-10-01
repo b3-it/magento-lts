88da867aac4c49d748d8ac65cb0661bf90d78196
Frank Rochlitzer <f.rochlitzer@b3-it.de>
2015-10-01 10:58:22 +0200

    +  Alle :: Egovs_Giropay +  Alle :: Egovs_Saferpay

---
+++ app/code/local/Egovs/Giropay/Block/Redirect.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Redirectblock für Giropay über Saferpay
+ *
+ * Redirect zu Saferpay
+ *
+ * @category   	Egovs
+ * @package    	Egovs_Giropay
+ * @name       	Egovs_Giropay_Block_Redirect
+ * @author 		Frank Rochlitzer <f.rochlitzer@trw-net.de>
+ * @author		René Sieberg <rsieberg@web.de>
+ * @copyright  	Copyright (c) 2011 EDV Beratung Hempel - http://www.edv-beratung-hempel.de
+ * @copyright  	Copyright (c) 2011 TRW-NET - http://www.trw-net.de
+ * @license		http://sid.sachsen.de OpenSource@SID.SACHSEN.DE
+ * 
+ */
+class Egovs_Giropay_Block_Redirect extends Egovs_Paymentbase_Block_Saferpay_Redirect
+{
+	
+	/**
+	 * Modulspezifischer Titel
+	 * 
+	 * @var string
+	 */
+	protected $_preTitle = 'Giropay';
+}
