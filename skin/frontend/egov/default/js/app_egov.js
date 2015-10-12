a3f924ba2dc6ae81c954ecce9f9da7c65480d242
Rene Mütterlein <r.muetterlein@b3-it.de>
2015-10-12 09:46:04 +0200

    *ALLE :: Basis-Layout für Sachsen :: Responsive-Design

---
+++ skin/frontend/egov/default/js/app_egov.js
@@ -0,0 +1,23 @@
+﻿$j(document).ready(function () {
+    // On viewports smaller than 1000px
+
+    if ($j('.page-header-container').length > 0) {
+        enquire.register('screen and (max-width: 785px)', {
+            match: function() {
+                // Welcome-Header eine Ebene nach oben schieben
+                $j('.welcome-header').appendTo( $j('.page-header-container') );
+
+                // Shop-Navigation in Mobil-Container schieben
+                $j('.nav-primary').appendTo( $j('#header-nav') );
+            },
+            unmatch: function() {
+                // Welcome-Header zurück zu den Skip-Links (Ursprung)
+                $j('.welcome-header').appendTo( $j('.skip-links') );
+
+                // Shop-Navigation zurück an Ursprung
+                $j('.nav-primary').appendTo( $j('#egov-nav') );
+            }
+        });
+    }
+
+});
