2549a786fc27e5db62e8e0b5ff41cd566eb9ee14
Rene Mütterlein <r.muetterlein@b3-it.de>
2015-10-13 07:28:28 +0200

    *ALLE :: Basis-Layout für Sachsen :: Responsive-Design

--- skin/frontend/egov/default/js/app_egov.js
+++ skin/frontend/egov/default/js/app_egov.js
@@ -1,20 +1,88 @@
-﻿$j(document).ready(function () {
-    // On viewports smaller than 1000px
+﻿var egov_break = {
+    lngSwitch: 760,    // Store-Language Switcher
+    welcome  : 785,    // Welcome
+    navbar   : 785     // Navigation
+}
 
-    if ($j('.page-header-container').length > 0) {
-        enquire.register('screen and (max-width: 785px)', {
+$j(document).ready(function () {
+
+    // Dynamische Skallierung der Welcome-Schrift
+    var resizeText = function() {
+        // aktuelle Breite des Containers
+        var pageContainer = $j('.page-header-container').width();
+        // Maximale Schriftgröße in Prozent
+        var preferredFont = 300;
+        // Maximale Breite des Inhalts in Pixel
+        var pageFullSize = 960;
+        // Skalierungs-Faktor
+        var scaleFactor = Math.sqrt(pageContainer) / Math.sqrt(pageFullSize);
+        // Berechnete Schrift-Größe
+        var newSize = preferredFont * scaleFactor;
+
+        $j('.header-position').css("font-size", newSize + '%');
+    };
+
+    $j(window).bind('resize', function(){
+        resizeText();
+    }).trigger('resize');
+
+    // Rechte Seitenspalte im 3-Spalten-Layout
+    if ($j('.main-container.col3-layout').length > 0) {
+        enquire.register('screen and (max-width: 1000px)', {
+            match: function () {
+                var rightColumn = $j('.col-right');
+                var colWrapper = $j('.col-main');
+
+                rightColumn.appendTo(colWrapper);
+            },
+            unmatch: function () {
+                var rightColumn = $j('.col-right');
+                var main = $j('.col-main');
+
+                rightColumn.prependTo(main);
+            }
+        });
+    }
+
+    // Language-Switcher umlegen
+    if ($j('#big-view-switcher').length > 0) {
+        $j('#big-view-switcher').appendTo( $j('.page-header-container') );
+
+        enquire.register('screen and (max-width: ' + egov_break.lngSwitch + 'px)', {
             match: function() {
-                // Welcome-Header eine Ebene nach oben schieben
-                $j('.welcome-header').appendTo( $j('.page-header-container') );
+                $j('#big-view-switcher').css('display', 'none');
+                $j('#small-view-switcher').css('display', 'block');
+            },
+            unmatch: function() {
+                $j('#big-view-switcher').css('display', 'block');
+                $j('#small-view-switcher').css('display', 'none');
+            }
+        });
+    }
 
-                // Shop-Navigation in Mobil-Container schieben
+    // Umlegen des Welcome-Header
+    if ($j('.page-header-container').length > 0) {
+        enquire.register('screen and (max-width: ' + egov_break.welcome + 'px)', {
+            match: function() {
+                // eine Ebene nach oben schieben
+                $j('.welcome-header').appendTo( $j('.page-header-container') );
+            },
+            unmatch: function() {
+                // zurück zu den Skip-Links (Ursprung)
+                $j('.welcome-header').appendTo( $j('.skip-links') );
+            }
+        });
+    }
+
+    // Umlegen der Shop-Navigation
+    if ( $j('.col-left').length > 0 && $j('.page-header-container').length > 0 ) {
+        enquire.register('screen and (max-width: ' + egov_break.navbar + 'px)', {
+            match: function() {
+                // in Mobil-Container schieben
                 $j('.nav-primary').appendTo( $j('#header-nav') );
             },
             unmatch: function() {
-                // Welcome-Header zurück zu den Skip-Links (Ursprung)
-                $j('.welcome-header').appendTo( $j('.skip-links') );
-
-                // Shop-Navigation zurück an Ursprung
+                // zurück an Ursprung
                 $j('.nav-primary').appendTo( $j('#egov-nav') );
             }
         });
