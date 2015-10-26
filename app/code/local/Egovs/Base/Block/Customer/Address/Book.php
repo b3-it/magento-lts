c8766621c8437a217a6d14b3bb1ce6ac5c647106
Rene Mütterlein <r.muetterlein@b3-it.de>
2015-10-26 10:09:14 +0100

    *ALLE :: Basis-Layout für Sachsen

--- app/code/local/Egovs/Base/Block/Customer/Address/Book.php
+++ app/code/local/Egovs/Base/Block/Customer/Address/Book.php
@@ -29,49 +29,45 @@
  *
  * @category   Egovs
  * @package    Egovs_Base
- * @author     
+ * @author
  */
 class Egovs_Base_Block_Customer_Address_Book extends Mage_Customer_Block_Address_Book
 {
-	public $AddressEditingIsDenied = false;
-	
-	public function rejectAddressEditing($address_id)
-	{
-		 $data = array('block'=> $this,"address_id"=>$address_id);
-		 Mage::dispatchEvent('egovs_base_customer_reject_address_editing',$data);
-		 $res = $this->AddressEditingIsDenied;
-		 $this->AddressEditingIsDenied = false;
-		 
-		 //für stammadresse
-		 if(!$res)
-		 {
-		 
-		 	 
-		 	 
-		 	$customer = Mage::getSingleton('customer/session')->getCustomer();
-		 	$baseAddress = $customer->getBaseAddress();
-		 	if($address_id != $baseAddress)
-		 	{
-		 		return false;
-		 	}
-		 	else {
-		 		if(!Mage::helper('egovsbase')->isModuleEnabled('Egovs_Vies'))
-		 		{
-		 			return true;
-		 		}
-			 	if($customer->getData('use_group_autoassignment') != 1)
-			 	{
-			 		return true;
-			 	}
-		 	}
-		 	
-		 }
-		 
-		 return $res;
-	}
-	
-	public function getLockedAddressText()
-	{
-		return $this->__('This address is used by additional service. For changeing contact our customer service please!');
-	}
+    public $AddressEditingIsDenied = false;
+
+    public function rejectAddressEditing($address_id)
+    {
+        $data = array('block'=> $this,"address_id"=>$address_id);
+        Mage::dispatchEvent('egovs_base_customer_reject_address_editing',$data);
+        $res = $this->AddressEditingIsDenied;
+		    //$this->AddressEditingIsDenied = false;
+
+		    //für stammadresse
+		    if(!$res)
+		    {
+		        $customer = Mage::getSingleton('customer/session')->getCustomer();
+		        $baseAddress = $customer->getBaseAddress();
+		        if($address_id != $baseAddress)
+		        {
+		            return false;
+		        }
+		        else
+		        {
+		            if(!Mage::helper('egovsbase')->isModuleEnabled('Egovs_Vies'))
+		            {
+		                return true;
+		            }
+		            if($customer->getData('use_group_autoassignment') != 1)
+		            {
+		                return true;
+		            }
+		        }
+		    }
+		    return $res;
+		}
+
+    public function getLockedAddressText()
+    {
+        return $this->__('This address is used by additional service. For changeing contact our customer service please!');
+    }
 }
