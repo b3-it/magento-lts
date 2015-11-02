6c579b6f60ad867ac801409485fc22ae3cd55226
Rene Mütterlein <r.muetterlein@b3-it.de>
2015-11-02 15:44:21 +0100

    *ALLE :: Basis-Layout für Sachsen; Zeichenbegrenzung in der Katalog-Ansicht; Zeichenbegrenzung bei zuletzt gesehenen Artikeln; Vorarbeit für Produktfile in Beschreibung

---
+++ app/code/local/Egovs/ProductFile/Model/Entity/Attribute/Frontend/Productfile.php
@@ -0,0 +1,18 @@
+﻿<?php
+/**
+ * Beschreibungsdatei für Produktinformationen
+ *
+ * @category	Egovs
+ * @package		Egovs_ProductFile
+ * @copyright	Copyright (c) 2015 B3-it System GmbH
+ * @license		http://sid.sachsen.de OpenSource@SID.SACHSEN.DE
+ */
+
+class Egovs_ProductFile_Model_Entity_Attribute_Frontend_Productfile extends Mage_Eav_Model_Entity_Attribute_Frontend_Abstract
+{
+    public function getValue() {
+        $_file  = $this->helper('productfile')->getProductFileFullName();
+        $_image = $this->helper('productfile')->getProductFileUploadDirectory() . DS . $this->getProductImage();
+        $_descr = $this->helper('productfile')->getProductFileDescription();
+    }
+}
