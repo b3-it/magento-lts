5de5ce9226c076f851569444e428b519eb09fd0b
Rene Mütterlein <r.muetterlein@b3-it.de>
2015-11-04 12:52:22 +0100

    + ALLE :: Beschreibungs-Datei in die Tabelle bei den Zusatzinformationen eingefügt; Renderer für FE erzeugt; Attribut für DB und Installer

---
+++ app/code/local/Egovs/ProductFile/data/productfile_setup/data-upgrade-0.1.2.0-2.0.1.0.php
@@ -0,0 +1,15 @@
+﻿<?php
+/* @var $installer Mage_Catalog_Model_Resource_Setup */
+$installer = $this;
+$connection = $installer->getConnection();
+
+$installer->startSetup();
+
+$entityTypeId = $installer->getEntityTypeId(Mage_Catalog_Model_Product::ENTITY);
+$attributeId = $installer->getAttributeId($entityTypeId, 'productfiledescription');
+
+$installer->updateAttribute($entityTypeId, $attributeId, 'frontend_input', 'media_image');
+$installer->updateAttribute($entityTypeId, $attributeId, 'is_visible_on_front', true);
+$installer->updateAttribute($entityTypeId, $attributeId, 'frontend_model', 'productfile/entity_attribute_frontend_productfile');
+
+$installer->endSetup();
