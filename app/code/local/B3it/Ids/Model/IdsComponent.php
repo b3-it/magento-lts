5107a0a41b972ed2f973c68502b3f746d538bbb3
Holger Kögel B3 IT <h.koegel@b3-it.de>
2015-12-03 13:48:02 +0100

    Libdir Typo für IDS

--- app/code/local/B3it/Ids/Model/IdsComponent.php
+++ app/code/local/B3it/Ids/Model/IdsComponent.php
@@ -41,7 +41,7 @@ class B3it_Ids_Model_IdsComponent extends Varien_Object
 		if($init)
 		{
 			$init->config['General']['base_path'] = Mage::getBaseDir('lib').DS.'IDS'.DS ;
-			$init->config['General']['filter_path'] = Mage::getBaseDir('lib').DS.'ids'.DS.'default_filter.xml' ;
+			$init->config['General']['filter_path'] = Mage::getBaseDir('lib').DS.'IDS'.DS.'default_filter.xml' ;
 			$init->config['General']['tmp_path'] = Mage::getBaseDir('var').DS.'ids'.DS ;
 			
 			$init->config['General']['use_base_path'] = false;
