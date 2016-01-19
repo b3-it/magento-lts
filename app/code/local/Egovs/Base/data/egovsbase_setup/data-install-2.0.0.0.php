a2a2f76983e18ca925fe74b68c05de12fe9e418b
Holger Kögel B3 It <h.koegel@b3-it.de>
2016-01-19 12:16:03 +0100

    Permissions für Variablen und Blöcke hinzu

--- app/code/local/Egovs/Base/data/egovsbase_setup/data-install-2.0.0.0.php
+++ app/code/local/Egovs/Base/data/egovsbase_setup/data-install-2.0.0.0.php
@@ -1,6 +1,8 @@
 <?php
 /* @var $this Mage_Eav_Model_Entity_Setup */
 $installer = $this;
+$installer->startSetup();
+
 
 $forms=array('customer_account_edit', 'customer_account_create', 'adminhtml_customer', 'checkout_register');
 $att = Mage::getModel('customer/attribute')->loadByCode('customer', 'company');
@@ -122,3 +124,28 @@ foreach ($stores as $store){
 	$block->setContent($content);
 	$block->save();
 }
+
+$installer->getConnection()->insertMultiple(
+		$installer->getTable('admin/permission_variable'),
+		array(
+				array('variable_name' => 'general/imprint/company_first', 'is_allowed' => 1),
+				array('variable_name' => 'general/imprint/street', 'is_allowed' => 1),
+				array('variable_name' => 'general/imprint/zip', 'is_allowed' => 1),
+				array('variable_name' => 'general/imprint/city', 'is_allowed' => 1),
+				array('variable_name' => 'general/imprint/email', 'is_allowed' => 1),
+		)
+);
+
+
+
+$installer->getConnection()->insertMultiple(
+		$installer->getTable('admin/permission_block'),
+		array(
+				array('block_name' => 'imprint/field', 'is_allowed' => 1),
+				array('block_name' => 'imprint/content', 'is_allowed' => 1),
+				array('block_name' => 'cms/block', 'is_allowed' => 1),
+				array('block_name' => 'symmetrics_impressum/impressum', 'is_allowed' => 1)
+		)
+);
+
+$installer->endSetup();
