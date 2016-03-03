7b8bcb961a4609c0abf78341f821968deea01447
Holger Kögel B3 IT <h.koegel@b3-it.de>
2016-03-03 11:51:50 +0100

    #2385; attribute zu custonmer_attribute_form hinzugefügt

---
+++ app/code/local/Egovs/Base/data/egovsbase_setup/data-upgrade-2.0.0.1-2.0.0.2.php
@@ -0,0 +1,37 @@
+<?php
+/* @var $this Mage_Eav_Model_Entity_Setup */
+$installer = $this;
+$installer->startSetup();
+
+
+$forms=array('adminhtml_customer_address','customer_address_edit','customer_register_address');
+$entityType = 'customer_address';
+$attributeCode = 'company2';
+$att = Mage::getModel('customer/attribute')->loadByCode($entityType, $attributeCode);
+if (!$att || $att->isEmpty()) {
+	Mage::throwException(sprintf('Attribute code "%s" for "%s" not found!', $attributeCode, $entityType));
+}
+$att->setData('used_in_forms', $forms)->setData('sort_order', '62')->save();
+
+$attributeCode = 'company3';
+$att = Mage::getModel('customer/attribute')->loadByCode($entityType, $attributeCode);
+if (!$att || $att->isEmpty()) {
+	Mage::throwException(sprintf('Attribute code "%s" for "%s" not found!', $attributeCode, $entityType));
+}
+$att->setData('used_in_forms', $forms)->setData('sort_order', '64')->save();
+
+$attributeCode = 'web';
+$att = Mage::getModel('customer/attribute')->loadByCode($entityType, $attributeCode);
+if (!$att || $att->isEmpty()) {
+	Mage::throwException(sprintf('Attribute code "%s" for "%s" not found!', $attributeCode, $entityType));
+}
+$att->setData('used_in_forms', $forms)->setData('sort_order', '200')->save();
+
+$attributeCode = 'email';
+$att = Mage::getModel('customer/attribute')->loadByCode($entityType, $attributeCode);
+if (!$att || $att->isEmpty()) {
+	Mage::throwException(sprintf('Attribute code "%s" for "%s" not found!', $attributeCode, $entityType));
+}
+$att->setData('used_in_forms', $forms)->setData('sort_order', '0')->save();
+
+$installer->endSetup();
