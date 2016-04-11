fbf5e8372df4855c4124ff025b4622bf3f797529
Rene Mütterlein <r.muetterlein@b3-it.de>
2016-04-11 11:21:27 +0200

    + STALA :: #2409 :: Skin hinzugefügt

---
+++ skin/frontend/egov/stala/scss/config.rb
@@ -0,0 +1,12 @@
+# note: this should never truly be refernced since we are using relative assets
+http_path = "/skin/frontend/egov/stala/"
+css_dir = "../css"
+sass_dir = "../scss"
+images_dir = "../images"
+javascripts_dir = "../../default/js"
+font_dir = "../../default/font"
+default_dir = "../../default"
+relative_assets = true
+
+output_style = :compressed
+environment = :production
