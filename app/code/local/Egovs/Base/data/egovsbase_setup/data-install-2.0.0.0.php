1a8c6b27f1f8431b5cac84602883ea5d0c56a5b4
Holger Kögel B3 IT <h.koegel@b3-it.de>
2016-04-18 14:36:18 +0200

    *alle:: Installer zur Migration angepasst

--- app/code/local/Egovs/Base/data/egovsbase_setup/data-install-2.0.0.0.php
+++ app/code/local/Egovs/Base/data/egovsbase_setup/data-install-2.0.0.0.php
@@ -137,16 +137,5 @@ $installer->getConnection()->insertMultiple(
 );
 
 
-/*
 
-$installer->getConnection()->insertMultiple(
-		$installer->getTable('admin/permission_block'),
-		array(
-				array('block_name' => 'imprint/field', 'is_allowed' => 1),
-				array('block_name' => 'imprint/content', 'is_allowed' => 1),
-				array('block_name' => 'cms/block', 'is_allowed' => 1),
-				array('block_name' => 'symmetrics_impressum/impressum', 'is_allowed' => 1)
-		)
-);
-*/
 $installer->endSetup();
