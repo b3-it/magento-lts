f53c809ade8f0ae7fc4512a12ba68855a0b47922
Frank Rochlitzer <f.rochlitzer@b3-it.de>
2016-06-29 17:52:23 +0200

    + Girosolution (unfinished)

---
+++ app/code/local/Egovs/Girosolution/Helper/Data.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Girosolution Helper Data
+ * 
+ * Überschreibt hier nur Egovs_Paymentbase_Helper_Data
+ *
+ * @category   	Egovs
+ * @package    	Egovs_Girosolution
+ * @name       	Egovs_Girosolution_Helper_Data
+ * @author 		Frank Rochlitzer <f.rochlitzer@b3-it.de>
+ * @copyright  	Copyright (c) 2016 B3 IT Systeme GmbH - http://www.b3-it.de
+ * @license		http://sid.sachsen.de OpenSource@SID.SACHSEN.DE
+ */
+class Egovs_Girosolution_Helper_Data extends Egovs_Paymentbase_Helper_Data
+{
+
+}
