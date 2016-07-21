077482f0246a0b4daaa609c6037ffecfe9861733
Rene Mütterlein <r.muetterlein@b3-it.de>
2016-07-21 13:15:55 +0200

    *BfR :: Design : Layout, Übersetzungen, Veranstaltungsprodukt

--- app/code/local/Bfr/EventRequest/controllers/IndexController.php
+++ app/code/local/Bfr/EventRequest/controllers/IndexController.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Bfr EventRequest
- * 
- * 
+ *
+ *
  * @category   	Bfr
  * @package    	Bfr_EventRequest
  * @name       	Bfr_EventRequest_IndexController
@@ -14,31 +14,29 @@ class Bfr_EventRequest_IndexController extends Mage_Checkout_Controller_Action
 {
     public function indexAction()
     {
-    	
-    	parent::preDispatch();
-    	$this->_preDispatchValidateCustomer();
-			
-    	
-    	$customer = Mage::getSingleton('customer/session')->getCustomer();
-    	if (!$customer || !$customer->getId()) {
-    		Mage::getSingleton('checkout/session')->addError("Login first!");
-    		$this->_redirect('checkout/cart');
-    		return $this;
-    	}
-    	
-    	$quote = Mage::getSingleton('checkout/session')->getQuote();
-    	$register =Mage::getModel('eventrequest/request');
-    	$errors = $register->registerQuote($quote, $customer);
-    	
-    	if(count($errors) > 0){
-    		foreach($errors as $error){
-    			Mage::getSingleton('checkout/session')->addError($error);
-    		}
-    		$this->_redirect('checkout/cart');
-    		return $this;
-    	}
-    	
-		Mage::getSingleton('checkout/session')->addSuccess("Your event request has been registered!");
-    	$this->_redirect('checkout/cart');
+        parent::preDispatch();
+        $this->_preDispatchValidateCustomer();
+
+        $customer = Mage::getSingleton('customer/session')->getCustomer();
+        if (!$customer || !$customer->getId()) {
+            Mage::getSingleton('checkout/session')->addError( $this->__("Please Login first!") );
+            $this->_redirect('checkout/cart');
+            return $this;
+        }
+
+        $quote = Mage::getSingleton('checkout/session')->getQuote();
+        $register = Mage::getModel('eventrequest/request');
+        $errors = $register->registerQuote($quote, $customer);
+
+        if(count($errors) > 0){
+            foreach($errors as $error){
+                Mage::getSingleton('checkout/session')->addError($error);
+            }
+            $this->_redirect('checkout/cart');
+            return $this;
+        }
+
+        Mage::getSingleton('checkout/session')->addSuccess( $this->__("Your event request has been registered!") );
+        $this->_redirect('checkout/cart');
     }
 }
