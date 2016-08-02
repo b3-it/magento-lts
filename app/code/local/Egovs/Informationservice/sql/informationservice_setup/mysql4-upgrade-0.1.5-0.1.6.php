5e9822b9e4a30831c0932bd2e8fefeaa460d511d
Frank Rochlitzer <f.rochlitzer@b3-it.de>
2016-08-02 19:39:32 +0200

    * Alle :: Egovs_Informationsservice : ZV_FM-698 Attributfehler aus Migration
    In Version 1.3 hieß das Attribut noch
    'informationservice_is_master_product'!
    Ab Version 1.6 wurde es in 'infoservice_is_master_product' umbenannt!

--- app/code/local/Egovs/Informationservice/sql/informationservice_setup/mysql4-upgrade-0.1.5-0.1.6.php
+++ app/code/local/Egovs/Informationservice/sql/informationservice_setup/mysql4-upgrade-0.1.5-0.1.6.php
@@ -4,6 +4,15 @@ $installer = $this;
 
 $installer->startSetup();
 
+/*
+ * ZV_FM-698
+ * In Version 1.3 hieß das Attribut noch 'informationservice_is_master_product'!
+ * Ab Version 1.6 wurde es in 'infoservice_is_master_product' umbenannt!
+ */
+if ($installer->getAttribute('catalog_product', 'informationservice_is_master_product')) {
+	$installer->updateAttribute('catalog_product', 'informationservice_is_master_product', array('attribute_code' => 'infoservice_is_master_product'));
+}
+
 $installer->updateAttribute('catalog_product', 'infoservice_is_master_product', 'is_configurable', false);
 
 $installer->endSetup(); 
