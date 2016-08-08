805a0f9557b7d4535def831fb47cf23d5edca7a8
Rene Mütterlein <r.muetterlein@b3-it.de>
2016-08-08 15:02:07 +0200

    *BfR :: Design : Produkt-Bild-Lupe deaktiviert; Anpassungen in der Produkt-Ansicht

--- skin/frontend/bfr/default/js/app_bfr.js
+++ skin/frontend/bfr/default/js/app_bfr.js
@@ -1,4 +1,19 @@
 // Allgemeine JS-Funktionen
+$j(document).ready(function(){
+    removeZomm();
+
+    $j('a.egov-product-image').click(function(){
+        removeZomm();
+    });
+});
+
+function removeZomm()
+{
+    setTimeout(function(){
+        $j('div.zoomContainer').remove();
+    }, 200);
+}
+
 function setTabIndex(arr)
 {
     $j.each(arr, function(element, tabindex){
