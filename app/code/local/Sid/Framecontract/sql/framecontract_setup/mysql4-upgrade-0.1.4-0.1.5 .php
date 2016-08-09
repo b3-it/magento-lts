ad69ab94b587a80ede4155d05d9d82b048ad8f49
Holger Kögel B3 IT <h.koegel@b3-it.de>
2016-08-09 16:16:15 +0200

    *sid::rahmenverträge --entwicklung

--- app/code/local/Sid/Framecontract/sql/framecontract_setup/mysql4-upgrade-0.1.4-0.1.5 .php
+++ app/code/local/Sid/Framecontract/sql/framecontract_setup/mysql4-upgrade-0.1.4-0.1.5 .php
@@ -6,6 +6,26 @@ $installer = $this;
 $installer->startSetup();
 
 
+$installer->addAttribute('catalog_product', 'framecontract_qty', array(
+		'label' => 'vereinbarte Liefermenge',
+		'input' => 'text',
+		'type' => 'int',
+		'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+		'visible' => true,
+		'default' => '0',
+		'group' =>'Framecontract'
+
+));
+
+$installer->addAttribute('customer_address', 'dap', array(
+		'label'			=> 'Deliver at Place',
+		'global' 		=> Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+		'visible'      	=> 1,
+		'required'     	=> 0,
+		'position'     	=> 1,
+		'sort_order'   	=> 40,
+));
+
 $installer->run("ALTER TABLE {$this->getTable('framecontract_transmit')} ADD `los_id` int default 0 ");
 $installer->run("ALTER TABLE {$this->getTable('framecontract_transmit')} ADD `note` varchar(255) NOT NULL default '' ");
 $installer->removeAttribute('catalog_product', 'framecontract');
