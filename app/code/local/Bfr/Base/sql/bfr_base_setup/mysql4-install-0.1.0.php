c8eff9fb0a907b9edb1b9721dce96d6ee0465a29
Holger Kögel B3 IT <h.koegel@b3-it.de>
2016-08-11 17:29:22 +0200

    *bfr::modul für einige Attribute hinzu

---
+++ app/code/local/Bfr/Base/sql/bfr_base_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,23 @@
+<?php
+
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+if (!$installer->getAttribute('catalog_product', 'name_prefix')) {
+	$installer->addAttribute('catalog_product', 'name_prefix', array(
+			'label' => 'Name Prefix',
+			'input' => 'text',
+			'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+			'visible' => true,
+			'required' => false,
+	));
+
+	//$installer->updateAttribute(Mage_Catalog_Model_Product::ENTITY, 'name_prefix', 'apply_to', Egovs_EventBundle_Model_Product_Type::TYPE_EVENTBUNDLE);
+}
+
+
+
+$installer->endSetup(); 
