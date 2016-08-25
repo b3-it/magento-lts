09a32915b8a918ba50c4e003607d4ffeedcf34f7
Holger Kögel B3 IT <h.koegel@b3-it.de>
2016-08-25 17:57:44 +0200

    *bfr::zusätzliche Attribute

---
+++ app/code/local/Bfr/EventManager/sql/eventmanager_setup/mysql4-upgrade-0.1.1-0.1.2.php
@@ -0,0 +1,66 @@
+<?php
+
+/* @var $installer Mage_Eav_Model_Entity_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+$installer->addAttribute('customer_address', 'titel', array(
+		'label'			=> 'Academic Titel',
+		'global' 		=> Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+		'visible'      	=> 1,
+		'required'     	=> 0,
+		'position'     	=> 1,
+		'sort_order'   	=> 31,
+));
+
+
+$installer->addAttribute('customer_address', 'position', array(
+		'label'        	=> 'Position',
+		'global' 		=> Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+		'visible'      	=> 1,
+		'required'     	=> 0,
+		'position'     	=> 1,
+		'sort_order'   	=> 32,
+));
+
+
+$entityType = 'customer_address';
+
+
+$forms=array('adminhtml_customer_address','customer_address_edit');
+$attributeCode = 'position';
+$att = Mage::getModel('customer/attribute')->loadByCode($entityType, $attributeCode);
+if (!$att || $att->isEmpty()) {
+	Mage::throwException(sprintf('Attribute code "%s" for "%s" not found!', $attributeCode, $entityType));
+}
+$att->setData('used_in_forms', $forms)->setData('sort_order', '1')->save();
+
+$attributeCode = 'titel';
+$att = Mage::getModel('customer/attribute')->loadByCode($entityType, $attributeCode);
+if (!$att || $att->isEmpty()) {
+	Mage::throwException(sprintf('Attribute code "%s" for "%s" not found!', $attributeCode, $entityType));
+}
+$att->setData('used_in_forms', $forms)->setData('sort_order', '0')->save();
+
+
+
+
+
+if (!$installer->getConnection()->tableColumnExists($installer->getTable('eventmanager/participant'), 'title')) {
+	$installer->run("
+			ALTER TABLE {$installer->getTable('eventmanager/participant')}
+			ADD COLUMN title varchar(255) NOT NULL default '';
+			");
+	
+}
+
+if (!$installer->getConnection()->tableColumnExists($installer->getTable('eventmanager/participant'), 'position')) {
+	$installer->run("
+			ALTER TABLE {$installer->getTable('eventmanager/participant')}
+			ADD COLUMN position varchar(255) NOT NULL default '';
+			");
+}
+
+
+$installer->endSetup();
