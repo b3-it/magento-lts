8ca32e835a5be8e156b74be6454a73c923e147f7
Rene Mütterlein <r.muetterlein@b3-it.de>
2016-08-30 12:09:11 +0200

    * Alle :: BaseBlock : Hinzufügen der Ausgaben für Template-Validator * BfR :: Layout : Validator-Ausgaben hinzugefügt

--- app/code/local/Egovs/Base/Block/Customer/Widget/Name.php
+++ app/code/local/Egovs/Base/Block/Customer/Widget/Name.php
@@ -12,45 +12,93 @@
 class Egovs_Base_Block_Customer_Widget_Name extends Mage_Customer_Block_Widget_Name
 {
     private $_method = 'register';
+    private $_helper = null;
 
     public function _construct() {
         parent::_construct();
 
         // default template location
         $this->setTemplate('customer/widget/name.phtml');
-    }
 
-    public function isFieldRequired($key)
-    {
-        $helper = null;
-        try {
-            $helper = Mage::helper('egovsbase/config');
-        } catch (Exception $e) {
-        }
-
-        if (is_null($helper)) {
-            return false;
-        }
-        return ($helper->isFieldRequired($key, $this->_method));
-    }
-
-    public function isFieldVisible($key)
-    {
-        $helper = null;
-        try {
-            $helper = $this->helper('egovsbase/config');
-        } catch (Exception $e) {
-        }
-
-        if (is_null($helper)) {
-            return true;
-        }
-
-        return $helper->isFieldVisible($key, $this->_method);
+        // Helper setzen
+        $this->setHelper();
     }
 
     /**
-     * Set current Controller-Method (Register, Checkout, ...)
+     * aktuellen Helper setzen
+     */
+    private function setHelper()
+    {
+        try {
+            $this->_helper = Mage::helper('egovsbase/config');
+        } catch (Exception $e) {
+        }
+
+        if (is_null($this->_helper)) {
+            return false;
+        }
+    }
+
+    /**
+     * Abfrage, ob ein bestimmtes Feld für Benutzerdaten pflicht ist
+     *
+     * @param string $key               Feldname
+     *
+     * @return bool
+     */
+    public function isFieldRequired($key)
+    {
+        if ( $this->_helper !== false ) {
+            return ($this->_helper->isFieldRequired($key, $this->_method));
+        }
+        else {
+            return false;
+        }
+    }
+
+    /**
+     * Abfrage, ob ein bestimmtes Feld für Benutzerdaten sichtbar ist
+     *
+     * @param string $key               Feldname
+     *
+     * @return bool
+     */
+    public function isFieldVisible($key)
+    {
+        if ( $this->_helper !== false ) {
+            return ($this->_helper->isFieldVisible($key, $this->_method));
+        }
+        else {
+            return false;
+        }
+
+    }
+
+    /**
+     * Wenn ein Benutzerfeld ein Pflichtfeld ist,
+     * so wird die Validator-Klasse im HTML benötigt
+     *
+     * @param string $key               Feldname
+     *
+     * @return string
+     */
+    public function getValidationClass($key)
+    {
+        $class = '';
+
+        if ( $this->_helper !== false ) {
+            if ( $this->_helper->isFieldVisible($key, $this->_method) ) {
+                if ( $this->_helper->isFieldRequired($key, $this->_method) ) {
+                    $class = 'required-entry';
+                }
+            }
+        }
+
+        return $class;
+    }
+
+    /**
+     * setzt die aktuelle Controller-Method (Register, Checkout, ...)
      */
     public function setMethod($method)
     {
@@ -58,6 +106,14 @@ class Egovs_Base_Block_Customer_Widget_Name extends Mage_Customer_Block_Widget_N
         return $this;
     }
 
+    /**
+     * Wenn ein Benutzerfeld ein Pflichtfeld ist,
+     * so wird im HTML das Markier-Element benötigt
+     *
+     * @param string $name               Feldname
+     *
+     * @return string
+     */
     public function getFieldRequiredHtml($name)
     {
         if ($this->isFieldRequired($name)) {
