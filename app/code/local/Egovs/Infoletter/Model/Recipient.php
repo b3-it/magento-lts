91127830d8f2937e2f8d0dbe4ca076b550504579
Holger Kögel B3 It <h.koegel@b3-it.de>
2016-09-15 14:55:26 +0200

    *sid:.cms js ausgelagert

--- app/code/local/Egovs/Infoletter/Model/Recipient.php
+++ app/code/local/Egovs/Infoletter/Model/Recipient.php
@@ -17,4 +17,18 @@ class Egovs_Infoletter_Model_Recipient extends Mage_Core_Model_Abstract
         parent::_construct();
         $this->_init('infoletter/recipient');
     }
+    
+    
+    public function createByCustomer(Mage_Customer_Model_Customer $customer, $queueId)
+    {
+    	$this->unsetData($this->getIdFieldName());
+    	$this->setData('message_id',$queueId);
+    	$this->setData('email',$customer->getEmail());
+    	$this->setData('prefix',$customer->getPrefix());
+    	$this->setData('firstname',$customer->getFirstname());
+    	$this->setData('lastname',$customer->getLastname());
+    	$this->setData('company',$customer->getCompany());
+    	
+    	return $this;
+    }
 }
