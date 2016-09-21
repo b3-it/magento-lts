5ecae6d7e16d590b66b5fd4080f432a297d53731
Holger Kögel B3 It <h.koegel@b3-it.de>
2016-09-21 11:30:58 +0200

    *sid::cms #2637 storeview speichern

--- app/code/local/Sid/Cms/Block/Adminhtml/Navi/New/Form.php
+++ app/code/local/Sid/Cms/Block/Adminhtml/Navi/New/Form.php
@@ -29,8 +29,8 @@ class Sid_Cms_Block_Adminhtml_Navi_New_Form extends Mage_Adminhtml_Block_Widget_
       
       $fieldset = $form->addFieldset('navi_form', array('legend'=>Mage::helper('sidcms')->__('Store information')));
       
-      $field =$fieldset->addField('store_id', 'multiselect', array(
-      		'name'      => 'stores[]',
+      $field =$fieldset->addField('store_id', 'select', array(
+      		'name'      => 'store_id',
       		'label'     => Mage::helper('cms')->__('Store View'),
       		'title'     => Mage::helper('cms')->__('Store View'),
       		'required'  => true,
