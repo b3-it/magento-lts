58938792e23c2a8283e26c7d6a6ba7388d7641af
Frank Rochlitzer <f.rochlitzer@b3-it.de>
2016-09-23 11:58:33 +0200

    * Alle :: Checkout : Unnötig überschriebene Funktion entfernt

--- app/code/local/Egovs/Checkout/Block/Multipage/Agreements.php
+++ app/code/local/Egovs/Checkout/Block/Multipage/Agreements.php
@@ -1,19 +1,20 @@
 <?php
-
-class Egovs_Checkout_Block_Multipage_Agreements extends Mage_Core_Block_Template
+/**
+ * Agreements
+ * 
+ * Nur noch für Abwärtskompatibilität!
+ *
+ * @category		Egovs
+ * @package			Egovs_Checkout
+ * @name			Egovs_Checkout_Block_Multipage_Agreements
+ * @author			Frank Rochlitzer <f.rochlitzer@b3-it.de>
+ * @copyright		Copyright (c) 2010 - 2016 B3 IT Systeme GmbH - http://www.b3-it.de
+ * @license			http://sid.sachsen.de OpenSource@SID.SACHSEN.DE
+ * @version			0.1.0.0
+ * @since			0.1.0.0
+ * @deprecated		use Mage_Checkout_Block_Agreements
+ *
+ */
+class Egovs_Checkout_Block_Multipage_Agreements extends Mage_Checkout_Block_Agreements
 {
-    public function getAgreements()
-    {
-        if (!$this->hasAgreements()) {
-            if (!Mage::getStoreConfigFlag('checkout/options/enable_agreements')) {
-                $agreements = array();
-            } else {
-                $agreements = Mage::getModel('checkout/agreement')->getCollection()
-                    ->addStoreFilter(Mage::app()->getStore()->getId())
-                    ->addFieldToFilter('is_active', 1);
-            }
-            $this->setAgreements($agreements);
-        }
-        return $this->getData('agreements');
-    }
 }
