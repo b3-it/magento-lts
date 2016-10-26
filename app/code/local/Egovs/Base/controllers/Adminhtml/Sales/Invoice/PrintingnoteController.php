e0b1277126258201a2b503c6423fa83e5b6a2fd5
Holger Kögel B3 It <h.koegel@b3-it.de>
2016-10-26 12:56:22 +0200

    *Alle::PrintingnoteController Fehler abgefangen

--- app/code/local/Egovs/Base/controllers/Adminhtml/Sales/Invoice/PrintingnoteController.php
+++ app/code/local/Egovs/Base/controllers/Adminhtml/Sales/Invoice/PrintingnoteController.php
@@ -11,19 +11,40 @@ class Egovs_Base_Adminhtml_Sales_Invoice_PrintingnoteController extends Mage_Adm
 		{
 			$model->setData('printnote1', $this->getRequest()->getParam('printnote1'));
 			$model->setData('printnote2', $this->getRequest()->getParam('printnote2'));
-			$model->save();
+			try{
+				$model->save();
+				$array = array(
+						'error'   => 'false',
+						'message' => Mage::helper('sales')->__('Data saved.'),
+				);
+				//$this->getResponse()->setHeader('Content-type', 'application/json');
+				$this->getResponse()->setBody( Mage::helper('core')->jsonEncode($array) );
+			}
+			catch(Exception $ex)
+			{
+				$array = array(
+						'error'   => 'true',
+						'message' => $ex->getMessage(),
+				);
+				//$this->getResponse()->setHeader('Content-type', 'application/json');
+				$this->getResponse()->setBody( Mage::helper('core')->jsonEncode($array) );
+			}
 			
-			$array = array(
-                         'error'   => 'false',
-					     'message' => Mage::helper('sales')->__('Data saved.'),
-			         );
-			//$this->getResponse()->setHeader('Content-type', 'application/json');
-			$this->getResponse()->setBody( Mage::helper('core')->jsonEncode($array) );
 		}
 	}
 	
 	protected function _isAllowed()
 	{
-		return Mage::getSingleton('admin/session')->isAllowed('sales/order/actions/printingnote');
+		$res =  Mage::getSingleton('admin/session')->isAllowed('sales/order/actions/printingnote');
+		if(!$res)
+		{
+			$array = array(
+					'error'   => 'true',
+					'message' => Mage::helper('sales')->__('Access denied!'),
+			);
+			//$this->getResponse()->setHeader('Content-type', 'application/json');
+			$this->getResponse()->setBody( Mage::helper('core')->jsonEncode($array) );
+		}
+		return $res;
 	}
 }
