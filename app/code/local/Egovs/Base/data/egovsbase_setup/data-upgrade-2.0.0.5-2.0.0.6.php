a8b89dbd8aa9e25ab131f030a031a872367b3f2b
Holger Kögel B3 IT <h.koegel@b3-it.de>
2016-11-10 16:58:21 +0100

    *sid::prüfen auf taxvat in installer

--- app/code/local/Egovs/Base/data/egovsbase_setup/data-upgrade-2.0.0.5-2.0.0.6.php
+++ app/code/local/Egovs/Base/data/egovsbase_setup/data-upgrade-2.0.0.5-2.0.0.6.php
@@ -6,9 +6,10 @@ $installer->startSetup();
 $entityType = 'customer_address';
 $attributeCode = 'taxvat';
 $att = Mage::getModel('customer/attribute')->loadByCode($entityType, $attributeCode);
-if (!$att || $att->isEmpty()) {
+if ($att && !$att->isEmpty()) {
 	Mage::throwException(sprintf('Attribute code "%s" for "%s" not found!', $attributeCode, $entityType));
+	$att->setData('used_in_forms', array())->save();
 }
-$att->setData('used_in_forms', array())->save();
+
 
 $installer->endSetup();
