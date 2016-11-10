62f60c8f70a9958aae76e7600e2942a5c7bccbb1
Holger Kögel B3 IT <h.koegel@b3-it.de>
2016-11-10 17:11:30 +0100

    *alle:.taxvat in installer prüfen

--- app/code/local/Egovs/Base/data/egovsbase_setup/data-upgrade-2.0.0.5-2.0.0.6.php
+++ app/code/local/Egovs/Base/data/egovsbase_setup/data-upgrade-2.0.0.5-2.0.0.6.php
@@ -7,7 +7,6 @@ $entityType = 'customer_address';
 $attributeCode = 'taxvat';
 $att = Mage::getModel('customer/attribute')->loadByCode($entityType, $attributeCode);
 if ($att && !$att->isEmpty()) {
-	Mage::throwException(sprintf('Attribute code "%s" for "%s" not found!', $attributeCode, $entityType));
 	$att->setData('used_in_forms', array())->save();
 }
 
