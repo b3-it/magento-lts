d041e6372cd92ccde8c816cdeaf5b8db9d17d09b
Frank Rochlitzer <f.rochlitzer@b3-it.de>
2016-11-17 16:10:06 +0100

    * Alle :: Vies : #2649 Migration von altem Schalter zur Deaktvierung der automatischen Kundengruppenzuordnung

--- app/code/local/Egovs/Vies/sql/egovsvies_setup/upgrade-1.6.1.0-1.8.1.0.php
+++ app/code/local/Egovs/Vies/sql/egovsvies_setup/upgrade-1.6.1.0-1.8.1.0.php
@@ -11,10 +11,9 @@ if (($attrId = $installer->getAttributeId('customer', 'use_group_autoassignment'
 	$coll->addAttributeToFilter($attrId, 0);
 	$coll->addAttributeToSelect($attrId);
 	foreach ($coll->getItems() as $customer) {
-		$customer->setDisableAutoGroupChange(true);
-		/* @var $resource Mage_Customer_Model_Resource_Customer */
-		$resource = $customer->getResource();
-		$resource->saveAttribute($customer, 'disable_auto_group_change');
+		$customer->setDisableAutoGroupChange(true)
+			->save()
+		;
 	}
 	$installer->removeAttribute('customer', 'use_group_autoassignment');
 }
