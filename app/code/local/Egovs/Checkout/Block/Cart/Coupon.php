ce06c018b41516f5be9cc2e7105c662fc404df94
Frank Rochlitzer <f.rochlitzer@b3-it.de>
2016-11-21 20:31:24 +0100

    * Alle :: Checkout : #2756 Prüfung auf aktive Coupons für 1.9 fixed
    Kundengruppen haben in 1.9 eine eigene Tabelle in SalesRule
    Magento-Natives nutzen

--- app/code/local/Egovs/Checkout/Block/Cart/Coupon.php
+++ app/code/local/Egovs/Checkout/Block/Cart/Coupon.php
@@ -10,46 +10,6 @@
  */
 class Egovs_Checkout_Block_Cart_Coupon extends Mage_Checkout_Block_Cart_Coupon
 {
-	/**
-	 * Store associated with rule entities information map
-	 *
-	 * @var array
-	 * @see Mage_SalesRule_Model_Resource_Rule_Collection
-	 */
-	protected $_associatedEntitiesMap = array(
-			'website' => array(
-					'associations_table' => 'salesrule/website',
-					'rule_id_field'      => 'rule_id',
-					'entity_id_field'    => 'website_id'
-			),
-			'customer_group' => array(
-					'associations_table' => 'salesrule/customer_group',
-					'rule_id_field'      => 'rule_id',
-					'entity_id_field'    => 'customer_group_id'
-			)
-	);
-	
-	/**
-	 * Retrieve correspondent entity information (associations table name, columns names)
-	 * of rule's associated entity by specified entity type
-	 *
-	 * @param string $entityType
-	 *
-	 * @return array
-	 */
-	protected function _getAssociatedEntityInfo($entityType)
-	{
-		if (isset($this->_associatedEntitiesMap[$entityType])) {
-			return $this->_associatedEntitiesMap[$entityType];
-		}
-	
-		$e = Mage::exception(
-				'Mage_Core',
-				Mage::helper('rule')->__('There is no information about associated entity type "%s".', $entityType)
-				);
-		throw $e;
-	}
-
 	protected function _isCouponRuleAvaillable($websiteId = null, $customerGroupId = null) {
 		if (is_null($websiteId)) {
 			$websiteId = $this->getQuote()->getStore()->getWebsiteId();
@@ -68,37 +28,8 @@ class Egovs_Checkout_Block_Cart_Coupon extends Mage_Checkout_Block_Cart_Coupon
 
 		$salesRuleCollection->addFieldToFilter('coupon_type', Mage_SalesRule_Model_Rule::COUPON_TYPE_SPECIFIC);
 		
-		$salesRuleCollection->addWebsiteFilter(intval($websiteId));
-        
-        $now = Mage::getModel('core/date')->date('Y-m-d');
-        
-        $entityInfo = $this->_getAssociatedEntityInfo('customer_group');
-        $connection = $salesRuleCollection->getConnection();
-        $salesRuleCollection->getSelect()
-        	->joinInner(
-        			array('customer_group_ids' => $salesRuleCollection->getTable($entityInfo['associations_table'])),
-        			$connection->quoteInto(
-        				'main_table.' . $entityInfo['rule_id_field']
-        				. ' = customer_group_ids.' . $entityInfo['rule_id_field']
-        				. ' AND customer_group_ids.' . $entityInfo['entity_id_field'] . ' = ?',
-        				(int)$customerGroupId
-        				),
-        			array()
-        		)
-        	//sind die nicht falsch herum --> CORE-BUG? @see addWebsiteGroupDateFilter
-//         	->where('from_date is null or from_date <= ?', $now)
-//         	->where('to_date is null or to_date >= ?', $now)
-        ;
-        
-        $salesRuleCollection->addIsActiveFilter();
-
-		$filterIsNull = array('null' => true);
-		$filterlteq = array('from' => $now);
-		$filtergteq = array('to'=> $now);
-
-		$salesRuleCollection->addFieldToFilter('from_date', array($filterIsNull,$filtergteq ))
-			->addFieldToFilter('to_date', array($filterIsNull, $filterlteq));
-
+		$salesRuleCollection->addWebsiteGroupDateFilter(intval($websiteId), $customerGroupId);
+		
 		if (Mage::getStoreConfig('dev/log/log_level') == Zend_Log::DEBUG) {
 			$debug = $salesRuleCollection->getSelectCountSql()->assemble();
 			Mage::log("Coupon SalesRuleSql::\n$debug", Zend_Log::DEBUG, Egovs_Helper::LOG_FILE);
