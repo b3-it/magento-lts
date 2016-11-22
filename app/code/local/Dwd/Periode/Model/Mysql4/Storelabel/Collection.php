8a0670704891ed0c6c3e6a3c9698ce5eedc0861a
Frank Rochlitzer <f.rochlitzer@b3-it.de>
2016-11-22 10:39:50 +0100

    * DWD :: Periode : Mehrsprachigkeit

---
+++ app/code/local/Dwd/Periode/Model/Mysql4/Storelabel/Collection.php
@@ -0,0 +1,15 @@
+<?php
+
+class Dwd_Periode_Model_Mysql4_Storelabel_Collection extends Mage_Core_Model_Mysql4_Collection_Abstract
+{
+	
+	
+	
+    public function _construct()
+    {
+        parent::_construct();
+        $this->_init('periode/storelabel');
+    }
+    
+    
+}
