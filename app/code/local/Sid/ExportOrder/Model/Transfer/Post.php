f45d99d674e06bb9552bea5227d3af9509171211
Holger Kögel B3 IT <h.koegel@b3-it.de>
2016-12-06 11:52:51 +0100

    *itw::ZV_FM-947

--- app/code/local/Sid/ExportOrder/Model/Transfer/Post.php
+++ app/code/local/Sid/ExportOrder/Model/Transfer/Post.php
@@ -10,6 +10,20 @@
  * @copyright  	Copyright (c) 2015 B3 It Systeme GmbH - http://www.b3-it.de
  * @license		http://sid.sachsen.de OpenSource@SID.SACHSEN.DE
  */
+/**
+ *  @method int getId()
+ *  @method setId(int $value)
+ *  @method int getVendorId()
+ *  @method setVendorId(int $value)
+ *  @method string getAddress()
+ *  @method setAddress(string $value)
+ *  @method string getPort()
+ *  @method setPort(string $value)
+ *  @method string getUser()
+ *  @method setUser(string $value)
+ *  @method string getPwd()
+ *  @method setPwd(string $value)
+ */
 class Sid_ExportOrder_Model_Transfer_Post extends Sid_ExportOrder_Model_Transfer
 {
     public function _construct()
@@ -20,25 +34,46 @@ class Sid_ExportOrder_Model_Transfer_Post extends Sid_ExportOrder_Model_Transfer
     
     public function send($content,$order = null)
     {
-    	$postfields = array();
-    	$postfields['data'] = ($content);
-    	  
+    	$output = "";
+    	try 
+    	{
     	    	
-    	$ch = curl_init();
-    	
-    	// Follow any Location headers
-    	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    	
-    	curl_setopt($ch, CURLOPT_URL, $this->getAddress().'xx');
-    	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    	
-    	// Alert cURL to the fact that we're doing a POST, and pass the associative array for POSTing.
-    	curl_setopt($ch, CURLOPT_POST, 1);
-    	
-    	curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
-    	
-    	$output = curl_exec($ch);
-    	curl_close($ch);
+	    	$filename =$order->getIncrementId().'_'.date('d-m-Y_H-i-s').'xml';
+	    	$path =  Mage::getBaseDir('tmp').DS. $filename;
+	    	
+	    	file_put_contents($path, $content);
+	    	$this->setLog('Erzeuge tmp Datei: '.$path);
+	    	$cfile = new CURLFile($filename,'application/xml','Filedata');
+	    	$ch = curl_init();
+	    	
+	    	// Follow any Location headers
+	    	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	    	
+	    	curl_setopt($ch, CURLOPT_URL, $this->getAddress());
+	    	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	    	curl_setopt($ch, CURLOPT_POST, 1);
+	    	
+	    	if(!empty($this->getUser())){
+	    		$this->setLog('setze Username: '. $this->getUser());
+	    		curl_setopt($ch,CURLOPT_PROXYUSERPWD,$this->getUser().':'.$this->getPwd());
+	    	}
+	    	
+	    	$data = array('file' => $cfile);
+	    	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	    	
+	    	$output = curl_exec($ch);
+	    	curl_close($ch);
+	    	if(file_exists($path))
+	    	{
+	    		unlink($path);
+	    		$this->setLog('Lösche tmp Datei: '.$path);
+	    	}
+    	}
+    	catch(Exception $ex)
+    	{
+    		$output = $ex->getMessage();
+    		Mage::logException($ex);
+    	}
     	
     	Sid_ExportOrder_Model_History::createHistory($order->getId(), 'per Post übertragen');
     	
