59855b4e65b45236e08698bffd6c26697d977560
h.mackowiak <h.mackowiak@PC-Hans.b3-it.local>
2016-12-13 13:58:38 +0100

    DWD: Fixe Stationauswahl mit Karte

--- skin/frontend/dwd/default/js/map-functions-list.js
+++ skin/frontend/dwd/default/js/map-functions-list.js
@@ -1,12 +1,9 @@
 function selectStation(id)
 {
-    $$('select#stationenListe option').each(function(o) {
-        if(o.readAttribute('value') == id) {
-	        o.selected = true;
-	        if($('quicksearch')) {$('quicksearch').value = '';}
-	            throw $break;
-	        }
-	});
+	var $el = $j('select#stationenListe > option[value="'+ id +'"]');
+    $j($el).attr('selected','selected');
+
+    $j( "select#stationenListe" ).selectmenu( "refresh" );
 }
 
 function selectStationSuggest(id)
