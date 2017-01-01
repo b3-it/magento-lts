fc33a47a9fb86e9672d96be845f51795bce374c8
Holger Kögel B3 It <h.koegel@b3-it.de>
2017-01-01 18:19:55 +0100

    **BKG::xmlbind für getCapabilllities

---
+++ lib/B3it/XmlBind/Wms13/Operationtype.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ *
+ * XML Bind  für Opentrans 2.1
+ * @category   	B3it
+ * @package    	B3it_XmlBind_Wms13_
+ * @name       	Operationtype
+ * @author 		Holger Kögel <h.koegel@b3-it.de>
+ * @copyright  	Copyright (c) 2016 B3 It Systeme GmbH - http://www.b3-it.de
+ * @license		http://sid.sachsen.de OpenSource@SID.SACHSEN.DE
+ */
+class B3it_XmlBind_Wms13_Operationtype extends B3it_XmlBind_Wms13_XmlObject
+{
+	
+	
+
+	
+
+	
+
+	
+
+
+
+
+
+	public function toXml($xml)
+	{
+		foreach($this->getAttributes() as $key => $value){
+			$xml->setAttribute($key,$value);
+		}
+		
+
+
+		return $xml;
+	}
+
+}
