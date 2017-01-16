b125e2144be6752db1d8f6bc24183e7d6cedf3eb
Rene Mütterlein <r.muetterlein@b3-it.de>
2017-01-16 15:59:03 +0100

    *DWD :: Design : #2708 : Darstellung der Überschriften im gesammten Shop auf normal gesetzt; Migration der DWD-Produkte auf die aktuelle Funktion der ePayBL-Anzeige im BE; ICD-Übersetzungen für Checkout

---
+++ app/code/local/Dwd/ConfigurableVirtual/data/configvirtual_setup/data-upgrade-0.0.1.3-0.0.1.4.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Haushalsparameter nur an bestimmte Produkte binden
+ *
+ * @category        Dwd
+ * @package         Dwd_ConfigurableVirtual
+ * @author 			Frank Rochlitzer <f.rochlitzer@b3-it.de>
+ * @copyright      	Copyright (c) 2016 B3 IT Systeme GmbH - http://www.b3-it.de
+ * @license        	http://sid.sachsen.de OpenSource@SID.SACHSEN.DE
+ */
+/* @var $this Egovs_Paymentbase_Model_Resource_Setup */
+$installer = $this;
+
+$fieldList = array(
+		'haushaltsstelle',
+		'objektnummer',
+		'objektnummer_mwst',
+		'href',
+		'href_mwst',
+		'buchungstext',
+		'buchungstext_mwst'
+);
+
+//gleiche Einstellungen wie für Preis
+$productList = array();
+
+if (Mage::helper ('core')->isModuleEnabled ('Dwd_ConfigurableVirtual')) {
+	$productList[] = Dwd_ConfigurableVirtual_Model_Product_Type_Configurable::TYPE_CONFIGURABLE_VIRTUAL;
+}
+
+// make these attributes applicable to special products
+foreach ($fieldList as $field) {
+    $applyTo = explode(',', $installer->getAttribute(Mage_Catalog_Model_Product::ENTITY, $field, 'apply_to'));
+    foreach ($productList as $product) {
+	    if (!in_array($product, $applyTo)) {
+	        $applyTo[] = $product;
+	    }
+    }
+    $installer->updateAttribute(Mage_Catalog_Model_Product::ENTITY, $field, 'apply_to', implode(',', $applyTo));
+}
+ 
