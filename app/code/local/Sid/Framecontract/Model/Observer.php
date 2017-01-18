7867d8d6a42f1f262b61d9a1cd557af909db30f0
Holger Kögel B3 It <h.koegel@b3-it.de>
2017-01-18 18:30:31 +0100

    *ITW::altlasten entfernt

--- app/code/local/Sid/Framecontract/Model/Observer.php
+++ app/code/local/Sid/Framecontract/Model/Observer.php
@@ -2,28 +2,6 @@
 
 class Sid_Framecontract_Model_Observer extends Mage_Core_Model_Abstract
 {
-   
-	
-    
-	/**
-	 * Mail an Lieferanten versenden falls neue Bestellung
-	 * @param unknown $observer
-	 */
-	public function onCreateOrder($observer)
-	{
-		return $this; //obsolet?
-		try 
-		{
-			$orders = $observer->getOrders();
-			//$address = $observer->getAddress();
-			$model = Mage::getModel('framecontract/order');
-			$model->sendOrderEmail($orders);
-		}
-		catch (Exception $e) {
-            Mage::logException($e);
-		}
-	}
- 
 	/**
 	 * 
 	 * @param unknown $observer
