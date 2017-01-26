2875ea107548c0055ca704569a805f4319e5873f
Holger Kögel B3 IT <h.koegel@b3-it.de>
2017-01-26 11:59:18 +0100

    *DWD::stationsliste als jason Erzeugen

--- app/code/local/Dwd/Stationen/Block/Catalog/Product/View/Map.php
+++ app/code/local/Dwd/Stationen/Block/Catalog/Product/View/Map.php
@@ -6,4 +6,15 @@ class Dwd_Stationen_Block_Catalog_Product_View_Map extends Dwd_Stationen_Block_C
 		return $this->getUrl('fstationen/liste/rss',array('product_id'=>$this->getProduct()->getId()));
 	}
     
+	
+	public function getStationenPos()
+	{
+		$res = array();
+		$list = $this->getStationsList();
+		foreach($list as $station){
+			//$res[] = array('name'=>$station->getName(), 'id'=>$station->getId());
+			$res[$station->getId()] = $station->getName();
+		}
+		return json_encode($res);
+	}
 }
