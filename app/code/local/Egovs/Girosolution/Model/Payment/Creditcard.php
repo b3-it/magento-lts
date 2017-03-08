f07dcd60068cd6a6a124c4e4b2c24333b8c53768
Frank Rochlitzer <f.rochlitzer@b3-it.de>
2017-03-08 18:08:14 +0100

    * Alle :: GiroCheckout : #2802 Konfigurierbarkeit der Verwendung von Pseudokartennummern

--- app/code/local/Egovs/Girosolution/Model/Payment/Creditcard.php
+++ app/code/local/Egovs/Girosolution/Model/Payment/Creditcard.php
@@ -87,8 +87,14 @@ class Egovs_Girosolution_Model_Payment_Creditcard extends Egovs_Paymentbase_Mode
 	 * @see Egovs_Paymentbase_Model_Girosolution::_getGirosolutionRedirectUrl()
 	 */
 	protected function _getGirosolutionRedirectUrl() {
-		//Es müsen immer Pseudokartennummern erzeugt werden.
-		$this->_fieldsArr['pkn'] = 'create';
+		/**
+		 * 20170308::Frank Rochlitzer
+		 * ZV_AM-813 ZV_AM-814 Unterstützung zur Konfiguration von Pseudokartennummern-Nutzung
+		 */
+		if (Mage::getStoreConfigFlag("payment/{$this->getCode()}/use_pseudo")) {
+			//Pseudokartennummern
+			$this->_fieldsArr['pkn'] = 'create';
+		}
 	}
 	
 	protected function _callSoapClientImpl($objSOAPClient, $wId, $mandantNr, $refId, $providerName) {
