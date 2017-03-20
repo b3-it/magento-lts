da14b7685c1a02ed36adf3f2a03be52a89c8478e
Holger Kögel B3 IT <h.koegel@b3-it.de>
2017-03-20 11:36:22 +0100

    *B3::configCompare

--- app/code/local/B3it/ConfigCompare/Block/Adminhtml/Widget/Grid/Column/Renderer/Diff.php
+++ app/code/local/B3it/ConfigCompare/Block/Adminhtml/Widget/Grid/Column/Renderer/Diff.php
@@ -19,8 +19,8 @@ class  B3it_ConfigCompare_Block_Adminhtml_Widget_Grid_Column_Renderer_Diff exten
         $string = htmlspecialchars($string);
         if(strlen($string) > 300){
 	        $id = rand(1,10000);
-	        $btn  = '<button id="btn_p_'.$id.'" type = "button" onclick="$(\''.$id.'\').removeClassName(\'col_diff_small\'); $(\'btn_p_'.$id.'\').hide(); $(\'btn_m_'.$id.'\').show();">+</button>';
-	        $btn2 = '<button id="btn_m_'.$id.'" type = "button" onclick="$(\''.$id.'\').addClassName(\'col_diff_small\'); $(\'btn_m_'.$id.'\').hide(); $(\'btn_p_'.$id.'\').show();" style="display:none;">-</button>';
+	        $btn  = '<button id="btn_p_'.$id.'" type = "button" onclick="$(\''.$id.'\').removeClassName(\'col_diff_small\'); $(\'btn_p_'.$id.'\').hide(); $(\'btn_m_'.$id.'\').show(); return false;">+</button>';
+	        $btn2 = '<button id="btn_m_'.$id.'" type = "button" onclick="$(\''.$id.'\').addClassName(\'col_diff_small\'); $(\'btn_m_'.$id.'\').hide(); $(\'btn_p_'.$id.'\').show();" style="display:none;  return false;">-</button>';
 	        $string = $btn.$btn2.'<div id="'.$id.'" class="col_diff_small">'. $string .'</div>';
         }
         return html_entity_decode($string,ENT_QUOTES,'UTF-8');
