3fedf162f4e72055449e380fa2f7095fee5ba7cf
Rene Mütterlein <r.muetterlein@b3-it.de>
2017-03-28 12:31:38 +0200

    *Adlerwarte :: Design : Skin aus dem Egov-Bereich entfernt und in separaten Skin ausgelagert

--- skin/frontend/adlerwarte/default/scss/config.rb
+++ skin/frontend/adlerwarte/default/scss/config.rb
@@ -1,12 +1,12 @@
 # note: this should never truly be refernced since we are using relative assets
-http_path = "/skin/frontend/egov/adlerwarte/"
+http_path = "/skin/frontend/adlerwarte/"
 css_dir = "../css"
 sass_dir = "../scss"
 images_dir = "../images"
-javascripts_dir = "../../default/js"
-font_dir = "../../default/font"
-default_dir = "../../default"
+javascripts_dir = "../js"
+font_dir = "../font"
 relative_assets = true
+
 sass_options = {:unix_newlines => true}
 
 output_style = :compressed
