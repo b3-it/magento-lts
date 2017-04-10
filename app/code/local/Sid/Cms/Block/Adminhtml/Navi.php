15e6e06ff2c3f6edd1451d551334d4e2861d239c
Holger Kögel B3 IT <h.koegel@b3-it.de>
2017-04-10 15:23:05 +0200

    *ITW:: ZV_FM-1153 Übersetzungen hinzu

--- app/code/local/Sid/Cms/Block/Adminhtml/Navi.php
+++ app/code/local/Sid/Cms/Block/Adminhtml/Navi.php
@@ -17,7 +17,7 @@ class Sid_Cms_Block_Adminhtml_Navi extends Mage_Adminhtml_Block_Widget_Grid_Cont
     $this->_controller = 'adminhtml_navi';
     $this->_blockGroup = 'sidcms';
     $this->_headerText = Mage::helper('sidcms')->__('CMS Navigation');
-    $this->_addButtonLabel = Mage::helper('sidcms')->__('Add Menu');
+    $this->_addButtonLabel = Mage::helper('sidcms')->__('Add Navigation Menu');
     parent::__construct();
   }
 }
