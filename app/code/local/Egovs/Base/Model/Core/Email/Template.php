d7d548b8db9c0529c2bd856aa2619c67f6bd8c58
Frank Rochlitzer <f.rochlitzer@b3-it.de>
2017-06-22 17:49:31 +0200

    * Anpassungen für Magento Version 1.9.3.3 (unfinished)
    app/code/local/Egovs Rewrites noch unfertig!
    Weitesgehend kompatibel mit PHP 7

--- app/code/local/Egovs/Base/Model/Core/Email/Template.php
+++ app/code/local/Egovs/Base/Model/Core/Email/Template.php
@@ -161,6 +161,7 @@ class Egovs_Base_Model_Core_Email_Template extends Mage_Core_Model_Email_Templat
 		if ($this->hasQueue() && $this->getQueue() instanceof Mage_Core_Model_Email_Queue) {
 			/** @var $emailQueue Egovs_Base_Model_Core_Email_Queue */
 			$emailQueue = $this->getQueue();
+			$emailQueue->clearRecipients();
 			$emailQueue->setMessageBody($text);
 			$emailQueue->setMessageParameters(array(
 					'subject'           => $subject,
