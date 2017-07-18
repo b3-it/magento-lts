4bfbb54a7b0a75617d87ee6caa1ef264a8fffb7f
Holger Kögel B3 IT <h.koegel@b3-it.de>
2017-07-18 14:17:23 +0200

    *GKA::Produkte Zeitgesteuert Aktivieren/Deaktivieren

---
+++ app/code/local/Gka/Product/Helper/Data.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *  Product Helper
+ *  @category Gka
+ *  @package  Gka_Product
+ *  @author Frank Rochlitzer <​f.rochlitzer@b3-it.de>
+ *  @author Holger Kögel <​h.koegel@b3-it.de>
+ *  @copyright Copyright (c) 2014 B3 IT Systeme GmbH
+ *  @license ​http://sid.sachsen.de OpenSource@SID.SACHSEN.DE
+ */ 
+class Gka_Product_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
