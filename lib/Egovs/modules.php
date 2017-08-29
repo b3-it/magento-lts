f0d9296650e3b078e4e434a23776917a28863550
René Mütterlein <r.muetterlein@b3-it.de>
2017-08-29 10:23:14 +0200

    *Alle :: Modul-Verwaltung : Mixed-Content korrigiert

--- lib/Egovs/modules.php
+++ lib/Egovs/modules.php
@@ -58,7 +58,7 @@ function set_header($title = 'Magento - Modulverwaltung')
             .min          {width:0%; height:0%;}
             .max          {width:100%; height:100%;}
         </style>
-        <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-latest.min.js"></script>
 ';
 }
 
