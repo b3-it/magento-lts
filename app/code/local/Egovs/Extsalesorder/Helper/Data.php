c097491658c6b4f048107411465705b36a3e543b
Holger Kögel B3 IT <h.koegel@b3-it.de>
2017-08-31 11:01:24 +0200

    #2895: filterCondition hinzu

--- app/code/local/Egovs/Extsalesorder/Helper/Data.php
+++ app/code/local/Egovs/Extsalesorder/Helper/Data.php
@@ -14,18 +14,30 @@
  */
 class Egovs_Extsalesorder_Helper_Data extends Mage_Core_Helper_Abstract
 {
-	/**
-	 * Übersetzt den Header einer Grid Column
-	 * 
-	 * @param array $args
-	 * 
-	 * @return array
-	 */
-	public function translate($args) {
-		if (array_key_exists('header', $args)) {
-			$args['header'] = $this->__($args['header']);
+	
+	
+	public function getBaseBalanceColumnParams()
+	{
+		return array(
+				'header' => $this->__('Balance (Base)'),
+				'index' => 'base_grand_total',
+				'type' => 'currency',
+				'renderer' => 'egovsbase/adminhtml_widget_grid_column_renderer_balance',
+				'currency' => 'base_currency_code', 
+				'filter_condition_callback' => array('Egovs_Extsalesorder_Helper_Data', 'filterbaseGrantTotalCondition'),
+		);
+	}
+	
+	public static function filterbaseGrantTotalCondition($collection, $column) {
+		if (!$value = $column->getFilter()->getValue()) {
+			return;
 		}
-		
-		return $args;
+	
+		if (isset($value['from'])){
+			$collection->getSelect()->where('(base_grand_total - ifnull(base_total_paid, 0))  >='.$value['from']*1);
+		}
+		if (isset($value['to'])){
+			$collection->getSelect()->where('(base_grand_total - ifnull(base_total_paid, 0))  <='.$value['to']*1);
+		}
 	}
 }
