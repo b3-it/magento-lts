8fec5e68069c255ff38f0fd5091df3c981a6316f
René Mütterlein <r.muetterlein@b3-it.de>
2017-09-06 12:18:57 +0200

    *Gka :: Design : Scrollbar reagiert auf Accordion mit Höhenanpassung; Mittelname aus dem Formular entfernt

--- skin/frontend/gka/default/js/overview.js
+++ skin/frontend/gka/default/js/overview.js
@@ -1,11 +1,16 @@
 $j(document).ready(function() {
-   	$j('#address').accordion({
+   	$j('.name-middlename').remove();
+	
+	$j('#address').accordion({
        	'active'     : false,
        	'collapsible': true,
        	'heightStyle': 'content',
        	'icons'      : {
    			'header'      : 'ui-icon-circle-plus',
    			'activeHeader': 'ui-icon-circle-minus'
+       	},
+       	'activate': function(event, ui) {
+       		$j('body').getNiceScroll().resize();
        	}
     });
 });
