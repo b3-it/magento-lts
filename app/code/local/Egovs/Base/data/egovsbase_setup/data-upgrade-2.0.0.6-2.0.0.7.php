6422a16a6f5db4fc0117fa9358202de952b6238e
René Mütterlein <r.muetterlein@b3-it.de>
2017-09-14 11:19:13 +0200

    *Alle :: Setup : den Quatsch löschen, den die TU-Freiberg in ihrem Shop eingestellt hat und damit das komplette Design in einigen Untershops "zerschießt"

--- app/code/local/Egovs/Base/data/egovsbase_setup/data-upgrade-2.0.0.6-2.0.0.7.php
+++ app/code/local/Egovs/Base/data/egovsbase_setup/data-upgrade-2.0.0.6-2.0.0.7.php
@@ -17,18 +17,37 @@ $design_path = array(
 );
 
 if ( $oldPackage == 'default') {
+    // eigene Designs löschen
+    $installer->run("DELETE FROM `core_config_data` WHERE `path` = 'design/package/name' AND `scope_id` > 0");
+    
+    // Default-Package setzen
     $installer->setConfigData('design/package/name', 'egov');
 
     foreach( $design_path AS $config_path ) {
         // alle "alten" Werte löschen
-        $installer->run("UPDATE `core_config_data` SET `value` = NULL WHERE `path` = '{$config_path}';");
+        $installer->run("DELETE FROM `core_config_data` WHERE `path` = '{$config_path}' AND `scope_id` > 0;");
 
         // neue Config setzen
         $installer->setConfigData($config_path, '');
     }
-
+    
+    // Spezielle Designs löschen
+    $installer->run("DELETE FROM `design_change`");
+    
 	$installer->setConfigData('design/header/logo_src', 'images/logo_sachsen.png');
 	$installer->setConfigData('design/header/logo_src_small', 'images/logo_sachsen_smartphone.png');
 }
+
+// DEMO-Hinweis löschen
+$installer->run("DELETE FROM `core_config_data` WHERE `path` = 'design/head/demonotice' AND `scope_id` > 0");
+
+// Seiten-Layout auf 2-Spaltig-Links setzen
+$_cmsTable = $installer->getTable('cms/page');
+if ($installer->tableExists($_cmsTable))
+{
+    $installer->run("UPDATE `{$_cmsTable}` SET `root_template` = 'two_columns_left' WHERE `root_template` = 'two_columns_right';");
+    $installer->run("UPDATE `{$_cmsTable}` SET `root_template` = 'two_columns_left' WHERE `root_template` = 'three_columns';");
+}
+
 
 $installer->endSetup();
