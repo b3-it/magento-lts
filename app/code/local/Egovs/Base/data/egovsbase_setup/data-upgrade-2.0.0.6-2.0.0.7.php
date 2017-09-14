88228f9d7895e462a589f3b192f61737f4e8992d
René Mütterlein <r.muetterlein@b3-it.de>
2017-09-14 08:34:45 +0200

    *Alle :: Design : Zurücksetzen der Design-Einstellungen auf Multi-Sites angepasst

--- app/code/local/Egovs/Base/data/egovsbase_setup/data-upgrade-2.0.0.6-2.0.0.7.php
+++ app/code/local/Egovs/Base/data/egovsbase_setup/data-upgrade-2.0.0.6-2.0.0.7.php
@@ -6,15 +6,27 @@ $installer->startSetup();
 // Default-Package nach "egov", wenn es "default" ist
 $oldPackage = Mage::getStoreConfig('design/package/name');
 
-if ( $oldPackage == 'default') {
-	$installer->setConfigData('design/package/name', 'egov');
+// Daten, welche im Design angepasst werden sollen
+// SELECT * FROM `core_config_data` WHERE `path` LIKE 'design/theme%'
+$design_path = array(
+    'design/theme/template',
+    'design/theme/skin',
+    'design/theme/layout',
+    'design/theme/default',
+    'design/theme/locale'
+);
 
-	$installer->setConfigData('design/theme/locale', '');
-	$installer->setConfigData('design/theme/template', '');
-	$installer->setConfigData('design/theme/skin', '');
-	$installer->setConfigData('design/theme/layout', '');
-	$installer->setConfigData('design/theme/default', '');
-	
+if ( $oldPackage == 'default') {
+    $installer->setConfigData('design/package/name', 'egov');
+
+    foreach( $design_path AS $config_path ) {
+        // alle "alten" Werte löschen
+        $installer->run("UPDATE `core_config_data` SET `value` = NULL WHERE `path` = '{$config_path}';");
+
+        // neue Config setzen
+        $installer->setConfigData($config_path, '');
+    }
+
 	$installer->setConfigData('design/header/logo_src', 'images/logo_sachsen.png');
 	$installer->setConfigData('design/header/logo_src_small', 'images/logo_sachsen_smartphone.png');
 }
