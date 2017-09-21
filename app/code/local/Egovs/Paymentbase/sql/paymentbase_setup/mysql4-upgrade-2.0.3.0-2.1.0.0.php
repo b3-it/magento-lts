2ac140b8c93f973c5cff38c6c6ca0c8142a98259
Frank Rochlitzer <f.rochlitzer@b3-it.de>
2017-09-21 11:06:55 +0200

    * Alle :: Paymentbase : Haushaltsparameter auf Store-Ebene ermöglichen

--- app/code/local/Egovs/Paymentbase/sql/paymentbase_setup/mysql4-upgrade-2.0.3.0-2.1.0.0.php
+++ app/code/local/Egovs/Paymentbase/sql/paymentbase_setup/mysql4-upgrade-2.0.3.0-2.1.0.0.php
@@ -26,7 +26,7 @@ foreach ($epayblAttributes as $attributeCode) {
     if (!$attributeId) {
         continue;
     }
-    $this->updateAttribute(Mage_Catalog_Model_Product::ENTITY, $attributeCode, 'global', Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_WEBSITE);
+    $installer->updateAttribute(Mage_Catalog_Model_Product::ENTITY, $attributeCode, 'global', Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_WEBSITE);
 }
 
 $installer->endSetup();
