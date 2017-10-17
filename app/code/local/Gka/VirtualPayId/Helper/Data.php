dfd4af5136a53f9c8af3bbb01730767fcc253398
René Mütterlein <r.muetterlein@b3-it.de>
2017-10-17 09:53:34 +0200

    *Gka :: FIX : PHP Fatal error: Class 'Mage_Gka_Virtualpayid_Helper_Data' not found *Gka :: PHP-Doc : Angaben hinzugefügt *Gka :: Design : Virtuelles Produkt per Default auf 2-Spaltig-Links gesetzt

--- app/code/local/Gka/VirtualPayId/Helper/Data.php
+++ app/code/local/Gka/VirtualPayId/Helper/Data.php
@@ -1,5 +1,14 @@
 <?php
-class Gka_VirtualPayId_Helper_Data extends Mage_Core_Helper_Data
+/**
+ *  Gka_VirtualPayId_Helper_Data
+ *  @category Gka
+ *  @package  Gka_VirtualPayId
+ *  @author Holger Kögel <​h.koegel@b3-it.de>
+ *  @copyright Copyright (c) 2014 B3 IT Systeme GmbH
+ *  @license ​http://sid.sachsen.de OpenSource@SID.SACHSEN.DE
+ */
+
+class Gka_VirtualPayId_Helper_Data extends Mage_Core_Helper_Abstract
 {
-	
+
 }
