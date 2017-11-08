77dd4ef21ba03da30309ae8be7759c8456fd55a2
Holger Kögel B3 It <h.koegel@b3-it.de>
2017-11-08 20:51:05 +0100

    *BKG::VirtualGeo + RAP hinzu

---
+++ app/code/local/Bkg/RegionAllocation/data/regionallocation_setup/data-install-0.0.1.0.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ *
+ *-- TODO:: kurze Beschreibung --
+ *
+ *
+ *
+ * @category        Egovs
+ * @package         Egovs_Ready
+ * @name            Egovs_Ready_Helper_Data
+ * @author 			Frank Rochlitzer <f.rochlitzer@b3-it.de>
+ * @copyright      	Copyright (c) 2010 - 2015 B3 IT Systeme GmbH - http://www.b3-it.de
+ * @license        	http://sid.sachsen.de OpenSource@SID.SACHSEN.DE
+ */
+/* @var $this Egovs_Paymentbase_Model_Resource_Setup */
+$installer = $this;
+
+$fieldList = array(
+		'haushaltsstelle',
+		'objektnummer',
+		'objektnummer_mwst',
+		'href',
+		'href_mwst',
+		'buchungstext',
+		'buchungstext_mwst',
+		'tax_class_id'
+);
+
+
+$productList = array(
+		'regionallocation',
+);
+
+foreach ($fieldList as $field) {
+	$applyTo = explode(',', $installer->getAttribute(Mage_Catalog_Model_Product::ENTITY, $field, 'apply_to'));
+	foreach ($productList as $product) {
+		if (!in_array($product, $applyTo)) {
+			$applyTo[] = $product;
+		}
+	}
+	$installer->updateAttribute(Mage_Catalog_Model_Product::ENTITY, $field, 'apply_to', implode(',', $applyTo));
+}
+
+$kst = Mage::getModel('regionallocation/koenigsteinerschluessel_kst');
+$kst->setName('2015')
+	->setActive(true)
+	->save();
+
+$regionPortions = array('BW'=>'0,1296662','BY'=>'0,1553327','BE'=>'0,0508324','BB'=>'0,0303655','HB'=>'0,0095331','HH'=>'0,0255752','HE'=>'0,0739885','MV'=>'0,020124','NI'=>'0,0933138','NRW'=>'0,2114424','RP'=>'0,0483089','SL'=>'0,0121111','SN'=>'0,0505577','ST'=>'0,0279941','SH'=>'0,0339074','TH'=>'0,026947');
+
+foreach( $regionPortions as $k=>$v)
+{
+	$kstv = Mage::getModel('regionallocation/koenigsteinerschluessel_kstvalue');
+	$kstv->setRegion($k)
+		->setPortion($v)
+		->setKstId($kst->getId())
+		->setHasTax('0');
+	if($k == 'BW'){
+		$kstv->setHasTax('1');
+	}
+	$kstv->save();
+}
+	
+
---
+++ app/code/local/Bkg/VirtualGeo/Model/Resource/Indexer/Stock.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magento.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magento.com for more information.
+ *
+ * @category    Mage
+ * @package     Mage_Bundle
+ * @copyright  Copyright (c) 2006-2016 X.commerce, Inc. and affiliates (http://www.magento.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+
+/**
+ * Bundle Stock Status Indexer Resource Model
+ *
+ * @category    Mage
+ * @package     Mage_Bundle
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
+class Bkg_VirtualGeo_Model_Resource_Indexer_Stock extends Mage_Bundle_Model_Resource_Indexer_Stock
+{
+   
+
+    
+    /**
+     * Prepare stock status per Bundle options, website and stock
+     *
+     * @param int|array $entityIds
+     * @param bool $usePrimaryTable use primary or temporary index table
+     * @return Mage_Bundle_Model_Resource_Indexer_Stock
+     */
+    protected function _prepareBundleOptionStockData($entityIds = null, $usePrimaryTable = false)
+    {
+        $this->_cleanBundleOptionStockData();
+        $idxTable = $usePrimaryTable ? $this->getMainTable() : $this->getIdxTable();
+        $adapter  = $this->_getWriteAdapter();
+        $select   = $adapter->select()
+            ->from(array('bo' => $this->getTable('bundle/option')), array('parent_id'));
+        $this->_addWebsiteJoinToSelect($select, false);
+        $status = new Zend_Db_Expr('MAX(' .
+                $adapter->getCheckSql('e.required_options = 0', 'i.stock_status', '0') . ')');
+        $select->columns('website_id', 'cw')
+            ->join(
+                array('cis' => $this->getTable('cataloginventory/stock')),
+                '',
+                array('stock_id')
+            )
+            ->joinLeft(
+                array('bs' => $this->getTable('bundle/selection')),
+                'bs.option_id = bo.option_id',
+                array()
+            )
+            ->joinLeft(
+                array('i' => $idxTable),
+                'i.product_id = bs.product_id AND i.website_id = cw.website_id AND i.stock_id = cis.stock_id',
+                array()
+            )
+            ->joinLeft(
+                array('e' => $this->getTable('catalog/product')),
+                'e.entity_id = bs.product_id',
+                array()
+            )
+            ->where('cw.website_id != 0')
+            ->group(array('bo.parent_id', 'cw.website_id', 'cis.stock_id', 'bo.option_id'))
+            ->columns(array(
+                'option_id' => 'bo.option_id',
+                'status'    => $status
+            ));
+
+        if (!is_null($entityIds)) {
+            $select->where('bo.parent_id IN(?)', $entityIds);
+        }
+
+        // clone select for bundle product without required bundle options
+        $selectNonRequired = clone $select;
+
+        $select->where('bo.required = ?', 1);
+        $selectNonRequired->where('bo.required != ?', 1)
+            ->having($status . ' = 1');
+        $query = $select->insertFromSelect($this->_getBundleOptionTable());
+        $adapter->query($query);
+
+        $query = $selectNonRequired->insertFromSelect($this->_getBundleOptionTable());
+        $adapter->query($query);
+
+        return $this;
+    }
+
+
+}
