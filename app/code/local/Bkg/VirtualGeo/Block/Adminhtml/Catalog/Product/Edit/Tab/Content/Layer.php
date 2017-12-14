f5d20426d08bedaad4c11cee01715572c35e745d
Holger Kögel B3 IT <h.koegel@b3-it.de>
2017-12-14 17:52:11 +0100

    *bkg::Virtualgeo Content BE überarbeitet

--- app/code/local/Bkg/VirtualGeo/Block/Adminhtml/Catalog/Product/Edit/Tab/Content/Layer.php
+++ app/code/local/Bkg/VirtualGeo/Block/Adminhtml/Catalog/Product/Edit/Tab/Content/Layer.php
@@ -46,8 +46,8 @@ class Bkg_VirtualGeo_Block_Adminhtml_Catalog_Product_Edit_Tab_Content_Layer exte
 			$res[] = new Varien_Object(array('id'=>$item->getComponentProductRelationId(),
 					'label'=>$item->getName(),
 					'entity_id'=>$item->getEntityId(),
-					'readonly'=>$item->getReadonly(),
-					'checked'=>$item->getChecked() ,
+					'readonly'=>boolval($item->getReadonly()),
+					'is_checked'=>boolval($item->getIsChecked()) ,
 					'pos' =>$item->getPos(),
 					'parent' => $parent != null? $parent->getComponentProductRelationId() : ''
 			));
