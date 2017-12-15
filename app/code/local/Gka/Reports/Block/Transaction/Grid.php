6002fc51cfa497b0dff9b943d833e9affa6d448c
Frank Rochlitzer <f.rochlitzer@b3-it.de>
2017-12-15 14:30:56 +0100

    * GKA :: Fixed Filter für Kassenzeichen

--- app/code/local/Gka/Reports/Block/Transaction/Grid.php
+++ app/code/local/Gka/Reports/Block/Transaction/Grid.php
@@ -1,159 +1,191 @@
 <?php
- /**
-  *
-  * @category   	Gka Reports
-  * @package    	Gka_Reports
-  * @name       	Gka_Reports_Block_Transaction_Grid
-  * @author 		Holger Kögel <h.koegel@b3-it.de>
-  * @copyright  	Copyright (c) 2017 B3 It Systeme GmbH - http://www.b3-it.de
-  * @license		http://sid.sachsen.de OpenSource@SID.SACHSEN.DE
-  */
+
+/**
+ *
+ * @category       Gka Reports
+ * @package        Gka_Reports
+ * @name           Gka_Reports_Block_Transaction_Grid
+ * @author         Holger Kögel <h.koegel@b3-it.de>
+ * @copyright      Copyright (c) 2017 B3 It Systeme GmbH - http://www.b3-it.de
+ * @license        http://sid.sachsen.de OpenSource@SID.SACHSEN.DE
+ */
 class Gka_Reports_Block_Transaction_Grid extends Mage_Adminhtml_Block_Widget_Grid
 {
-	
-	protected function _getUrlModelClass()
-	{
-		return 'core/url';
-	}
-	
-  public function __construct()
-  {
-      parent::__construct();
-      $this->setId('gka_transactionGrid');
-      $this->setDefaultSort('real_order_id');
-      $this->setDefaultDir('DESC');
-      $this->setSaveParametersInSession(true);
-      $this->setUseAjax(true);
-     
-  }
 
-  protected function _prepareCollection()
-  {
-  	$collection = Mage::getResourceModel('sales/order_grid_collection');
+    protected function _getUrlModelClass() {
+        return 'core/url';
+    }
 
-  
-  	$userId = intval(Mage::getSingleton('customer/session')->getCustomerId());
-  	
-  	$collection->getSelect()
-  	->joinleft(array('payment'=>$collection->getTable('sales/order_payment')),'payment.parent_id=main_table.entity_id',array('kassenzeichen'))
-   	->where('customer_id = ' . $userId)
-   	->where('store_id = '. intval(Mage::app()->getStore()->getId()))
-  	;
-  		
-  	//die($collection->getSelect()->__toString());
-  	
-    $this->setCollection($collection);
-    return parent::_prepareCollection();
-  }
-
-  protected function _prepareColumns()
-  {
-  	$this->addColumn('real_order_id', array(
-  			'header'=> Mage::helper('sales')->__('Order #'),
-  			'width' => '80px',
-  			'type'  => 'text',
-  			'index' => 'increment_id',
-  	));
-  	
-  	$this->addColumn('kassenzeichen', array(
-  			'header' => Mage::helper('sales')->__('Kassenzeichen'),
-  			'index' => 'kassenzeichen',
-  			'type' => 'text',
-  			'width' => '100px',
-  	));
-  	
-  	$this->addColumn('created_at', array(
-  			'header' => Mage::helper('sales')->__('Purchased On'),
-  			'index' => 'created_at',
-  			'type' => 'datetime',
-  			'width' => '100px',
-  	));
-  	
-  	$this->addColumn('billing_name', array(
-  			'header' => Mage::helper('sales')->__('Bill to Name'),
-  			'index' => 'billing_name',
-  	));
-  	
-
-  	
-    $this->addColumn('base_grand_total', array(
-            'header' => Mage::helper('sales')->__('G.T. (Base)'),
-            'index' => 'base_grand_total',
-            'type'  => 'currency',
-            'currency' => 'base_currency_code',
-    		'total'     => 'sum',
-        ));
-  	
-  	$this->addColumn('status', array(
-  			'header' => Mage::helper('sales')->__('Status'),
-  			'index' => 'status',
-  			'type'  => 'options',
-  			'width' => '70px',
-  			'options' => Mage::getSingleton('sales/order_config')->getStatuses(),
-  	));
-  	 
-  	$this->addColumn('payment_method', array(
-  			'header'    => Mage::helper('sales')->__('Payment Method'),
-  			'index'     => 'payment_method',
-  			'type'      => 'options',
-  			'width'     => '130px',
-  			'options'   => Mage::helper('gka_reports')->getActivePaymentMethods(),
-  	));
-  	
-
-
-		$this->addExportType('*/*/exportCsv', Mage::helper('gka_reports')->__('CSV'));
-		$this->addExportType('*/*/exportXml', Mage::helper('gka_reports')->__('XML'));
-		$this->setCountTotals(true);
-      return parent::_prepareColumns();
-  }
-
-	  /**
-	   * Retrieve totals
-	   *
-	   * @return Varien_Object
-	   */
-	  public function getTotals()
-	  {
-	  	return $this->_varTotals;
-	  }
-
-	public function getGridUrl($params = array())
-    {
-    	if (!isset($params['_current'])) {
-    		$params['_current'] = true;
-    	}
-    	$params['_store'] = Mage::app()->getStore();
-    	return $this->getUrl('*/*/grid', $params);
+    public function __construct() {
+        parent::__construct();
+        $this->setId('gka_transactionGrid');
+        $this->setDefaultSort('real_order_id');
+        $this->setDefaultDir('DESC');
+        $this->setSaveParametersInSession(true);
+        $this->setUseAjax(true);
 
     }
-    
-    protected function _afterLoadCollection()
-    {
-    	$data = array();
-    	foreach($this->getCollection() as $item)
-    	{
-    		foreach($this->getColumns() as $col)
-    		{
-    			if($col->getTotal())
-    			{
-    				if(!isset($data[$col->getId()])){
-    					$data[$col->getId()] = $item->getData($col->getIndex());
-    				}else {
-    					$data[$col->getId()] += $item->getData($col->getIndex());
-    				}
-    			}
-    		}
-    	}
-    	
-    	$helper = Mage::helper('core');
-    	foreach($data as $key=> $value)
-    	{
-    		$data[$key] = $helper->currency($value, true, false);
-    	}
-    	
-    	
-    	$this->setTotals(new Varien_Object($data));
+
+    protected function _prepareCollection() {
+        $collection = Mage::getResourceModel('sales/order_grid_collection');
+
+
+        $userId = intval(Mage::getSingleton('customer/session')->getCustomerId());
+
+        $collection->getSelect()
+            ->joinleft(array('payment' => $collection->getTable('sales/order_payment')), 'payment.parent_id=main_table.entity_id', array('kassenzeichen'))
+            ->where('customer_id = ' . $userId)
+            ->where('main_table.store_id = ' . intval(Mage::app()->getStore()->getId()));
+
+        $this->setCollection($collection);
+
+        //$this->_addCostUnits();
+
+        return parent::_prepareCollection();
+    }
+
+    protected function _addCostUnits() {
+        //TODO: 20171215: Frank Rochlitzer:: Funktioniert nicht so einfach, Transaktionen/Bestellungen können mehrere Produkte enthalten!!!
+        /* @var $catalog Mage_Catalog_Model_Resource_Product */
+        $catalog = Mage::getResourceSingleton('catalog/product');
+        $kfr = array();
+        $kfr[] = $catalog->getAttribute('haushaltsstelle');
+        $kfr[] = $catalog->getAttribute('objektnummer');
+
+        foreach ($kfr as $attribute) {
+            if (!$attribute) {
+                continue;
+            }
+            $alias = sprintf('_left_%s', $attribute->getAttributeCode());
+            /** @var $collection Mage_Sales_Model_Resource_Order_Grid_Collection */
+            $collection = $this->getCollection();
+            $collection->getSelect()
+                ->joinLeft(array($alias => $attribute->getBackendTable()),
+                    sprintf('%1$s.attribute_id = %2$s and %1$s.entity_id = main_table.product_id', $alias, $attribute->getAttributeId()),
+                    array($attribute->getAttributeCode() => 'value')
+                );
+        }
+    }
+
+    protected function _prepareColumns() {
+        $this->addColumn('real_order_id', array(
+            'header' => Mage::helper('sales')->__('Order #'),
+            'width' => '80px',
+            'type' => 'text',
+            'index' => 'increment_id',
+        ));
+
+        /* @var $catalog Mage_Catalog_Model_Resource_Product */
+        $catalog = Mage::getResourceSingleton('catalog/product');
+        /*
+         * TODO: 20171215: Frank Rochlitzer:: Funktioniert nicht so einfach, Transaktionen/Bestellungen können mehrere Produkte enthalten!!!
+        $this->addColumn('haushaltsstelle',
+            array(
+                'header' => Mage::helper('extreport')->__('Haushaltsstelle'),
+                'index' => 'haushaltsstelle',
+                'filter_index' => sprintf('_left_%s.value', $catalog->getAttribute('haushaltsstelle')->getAttributeCode()),
+            )
+        );
+
+        $this->addColumn('objnumber',
+            array(
+                'header' => Mage::helper('extreport')->__('Object number'),
+                'index' => 'objektnummer',
+                'filter_index' => sprintf('_left_%s.value', $catalog->getAttribute('objektnummer')->getAttributeCode()),
+            )
+        );
+        */
+
+        $this->addColumn('kassenzeichen', array(
+            'header' => Mage::helper('sales')->__('Kassenzeichen'),
+            'index' => 'kassenzeichen',
+            'type' => 'text',
+            'width' => '100px',
+            'filter_index' => 'payment.kassenzeichen',
+        ));
+
+        $this->addColumn('created_at', array(
+            'header' => Mage::helper('sales')->__('Purchased On'),
+            'index' => 'created_at',
+            'type' => 'datetime',
+            'width' => '100px',
+        ));
+
+        $this->addColumn('billing_name', array(
+            'header' => Mage::helper('sales')->__('Bill to Name'),
+            'index' => 'billing_name',
+        ));
+
+        $this->addColumn('base_grand_total', array(
+            'header' => Mage::helper('sales')->__('G.T. (Base)'),
+            'index' => 'base_grand_total',
+            'type' => 'currency',
+            'currency' => 'base_currency_code',
+            'total' => 'sum',
+        ));
+
+        $this->addColumn('status', array(
+            'header' => Mage::helper('sales')->__('Status'),
+            'index' => 'status',
+            'type' => 'options',
+            'width' => '70px',
+            'options' => Mage::getSingleton('sales/order_config')->getStatuses(),
+        ));
+
+        $this->addColumn('payment_method', array(
+            'header' => Mage::helper('sales')->__('Payment Method'),
+            'index' => 'payment_method',
+            'type' => 'options',
+            'width' => '130px',
+            'options' => Mage::helper('gka_reports')->getActivePaymentMethods(),
+        ));
+
+
+        $this->addExportType('*/*/exportCsv', Mage::helper('gka_reports')->__('CSV'));
+        $this->addExportType('*/*/exportXml', Mage::helper('gka_reports')->__('XML'));
+        $this->setCountTotals(true);
+        return parent::_prepareColumns();
+    }
+
+    /**
+     * Retrieve totals
+     *
+     * @return Varien_Object
+     */
+    public function getTotals() {
+        return $this->_varTotals;
+    }
+
+    public function getGridUrl($params = array()) {
+        if (!isset($params['_current'])) {
+            $params['_current'] = true;
+        }
+        $params['_store'] = Mage::app()->getStore();
+        return $this->getUrl('*/*/grid', $params);
+
+    }
+
+    protected function _afterLoadCollection() {
+        $data = array();
+        foreach ($this->getCollection() as $item) {
+            foreach ($this->getColumns() as $col) {
+                if ($col->getTotal()) {
+                    if (!isset($data[$col->getId()])) {
+                        $data[$col->getId()] = $item->getData($col->getIndex());
+                    } else {
+                        $data[$col->getId()] += $item->getData($col->getIndex());
+                    }
+                }
+            }
+        }
+
+        $helper = Mage::helper('core');
+        foreach ($data as $key => $value) {
+            $data[$key] = $helper->currency($value, true, false);
+        }
+
+
+        $this->setTotals(new Varien_Object($data));
     }
 
 
