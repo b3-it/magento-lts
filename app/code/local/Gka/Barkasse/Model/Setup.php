b4669a6d66708097f41134d2411e0a7bf22bd111
Holger Kögel B3 IT <h.koegel@b3-it.de>
2017-12-18 12:46:12 +0100

    *gka::setup für barkasse

---
+++ app/code/local/Gka/Barkasse/Model/Setup.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 
+ *  Setup Module zur Vereinfachung des Aufrufs bei der Intallation
+ *  @category Egovs
+ *  @package  Egovs_Pdftemplate
+ *  @author Frank Rochlitzer <​f.rochlitzer@b3-it.de>
+ *  @author Holger Kögel <​h.koegel@b3-it.de>
+ *  @copyright Copyright (c) 2014 B3 IT Systeme GmbH
+ *  @license ​http://sid.sachsen.de OpenSource@SID.SACHSEN.DE
+ */
+class Gka_Barkasse_Model_Setup extends Egovs_Pdftemplate_Model_Setup
+{
+	
+} 
