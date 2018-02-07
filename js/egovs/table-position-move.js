40c5d28c8a426e444c1fd0d64e6897c1d32cebb7
René Mütterlein <r.muetterlein@b3-it.de>
2018-02-07 14:56:48 +0100

    *Bkg :: Design : Tabelle-Zeilen können gelöscht werden

--- js/egovs/table-position-move.js
+++ js/egovs/table-position-move.js
@@ -12,6 +12,10 @@ $j(document).ready(function(){
         else if ( $j(this).is('.position-down') ) {
             row.insertAfter( row.next() );
         }
+        else if ( $j(this).is('.position-delete') ) {
+            // eventuell die gelöschten IDs merken
+            row.remove();
+        }
 
         // Positionen neu setzen
         setNewPositions();
@@ -20,20 +24,31 @@ $j(document).ready(function(){
         deactivateUnlogicalButtons();
     });
 
+    // Unbenutzbare Buttons setzen
     deactivateUnlogicalButtons();
 });
 
+/**
+ * bei allen Buttons das Attribut "deaktiviert" entfernen
+ */
 function activateAllButtons()
 {
     $j('table.data tbody button').removeAttr('disabled');
 }
 
+/**
+ * in der ersten Zeile den Button "up" deaktivieren
+ * in der letzten Zeile den Button "down" deaktivieren
+ */
 function deactivateUnlogicalButtons()
 {
     $j('table.data tbody tr:first-child button.position-up').attr('disabled', 'disabled');
     $j('table.data tbody tr:last-child button.position-down').attr('disabled', 'disabled');
 }
 
+/**
+ * Alle Positionen der Zeilen neu setzen
+ */
 function setNewPositions()
 {
     $j('table.data tbody input.position').each(function(index){
