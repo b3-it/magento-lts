bf2c4915030f6840b01dc5108b9a7587c545c3c7
René Mütterlein <r.muetterlein@b3-it.de>
2018-02-07 13:16:28 +0100

    *Bkg :: Design : Tabellen und Grund-Design der Lizens-Verwaltungs-Tabs korrigiert

--- app/code/local/Bkg/License/Block/Adminhtml/Master/Edit/Tab/Toll.php
+++ app/code/local/Bkg/License/Block/Adminhtml/Master/Edit/Tab/Toll.php
@@ -10,32 +10,40 @@
  */
 class Bkg_License_Block_Adminhtml_Master_Edit_Tab_Toll extends Mage_Adminhtml_Block_Widget_Form
 {
-	
+
 	protected function x_construct()
 	{
 		parent::_construct();
 		$this->setTemplate('bkg/license/master/edit/tab/toll.phtml');
 	}
-	
+
 	protected function _prepareForm()
 	{
 		$form = new Varien_Data_Form();
 		$this->setForm($form);
-		$fieldset = $form->addFieldset('unit_form', array('legend'=>Mage::helper('bkg_orgunit')->__(' Unit information')));
-	
+
+		// Tabelle mehrspaltig
+		$fieldset = $form->addFieldset('unit_form', array(
+				'class' => 'grid',
+				'legend' => Mage::helper('bkg_orgunit')->__('Unit information')
+		));
+
 		$values = Mage::getModel('customer/customer_attribute_source_group')->getAllOptions();
 		$fieldset->addType('ol','Egovs_Base_Block_Adminhtml_Widget_Form_Ol');
 		$fieldset->addField('shortname', 'ol', array(
-				'label'     => Mage::helper('bkg_orgunit')->__('Kurzname'),
-			
+				'label'     => Mage::helper('bkg_orgunit')->__('Short name'),
 				//'class'     => 'required-entry',
 				//'required'  => true,
 				'name'      => 'shortname',
 				'values' =>$values,
 				'value' => array(array('value'=>1,'pos'=>20),array('value'=>2,'pos'=>10))
 		));
-		
-		
+
+		// Zusätzliche Bedienelemente
+		$fieldset = $form->addFieldset('other_form', array(
+			'legend' => Mage::helper('bkg_license')->__('Other Fields')
+		));
+
 		$fieldset->addField('customer_groups', 'multiselect', array(
 				'label'     => Mage::helper('bkg_license')->__('Customer Groups'),
 				//'class'     => 'required-entry',
@@ -43,7 +51,7 @@ class Bkg_License_Block_Adminhtml_Master_Edit_Tab_Toll extends Mage_Adminhtml_Bl
 				'name'      => 'customer_groups',
 				'values' => $values
 		));
-		
+
 		$fieldset->addField('name', 'text', array(
 				'label'     => Mage::helper('bkg_license')->__('Name'),
 				//'class'     => 'required-entry',
@@ -51,18 +59,18 @@ class Bkg_License_Block_Adminhtml_Master_Edit_Tab_Toll extends Mage_Adminhtml_Bl
 				'name'      => 'name',
 		));
 	}
-	
-	
+
+
     public function getTollCollection()
     {
     	$collection = Mage::getModel('bkg_tollpolicy/toll')->getCollection();
     	$res = array();
-    	
+
     	foreach($collection as $item)
     	{
     		$res[$item->getId()] = $item->getName();
     	}
-    	
+
     	return $res;
     }
 }
