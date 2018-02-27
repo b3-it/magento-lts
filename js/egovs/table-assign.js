ccd26304ae14faf0ad98805795bd924cf00a3560
René Mütterlein <r.muetterlein@b3-it.de>
2018-02-27 12:24:47 +0100

    *BfR :: Admin-JS : Löschfunktion für Adressen implementiert

--- js/egovs/table-assign.js
+++ js/egovs/table-assign.js
@@ -4,54 +4,62 @@ function assignTable(tableId,name)
 	this.name = name;
 	this.nextID  = 0;
 	this.inputtype = 'hidden';
-	this.deleteRow = function(id){
-		 $j('#delete-' + this.tableId + '-' + id).val(1);
-		 $j('#row-'+this.tableId+'-'+id).hide();
-	};
+
+	this.deleteRow = function(rowId) {
+		var thisId = parseInt(rowId);
+
+		if ( isNaN(thisId) || thisId <= 0 ) {
+			return;
+		}
+
+		$j('#delete-' + this.tableId + '-' + thisId).val('1');
+		$j('#row-'    + this.tableId + '-' + thisId).toggle();
+	}
+
 	this.appendRow = function(data){
-		
+
 		if(!data){
 			var data = new Object();
 		}
-		
+
 		if(!data.left_value){
 			data.left_value = $j('#left-assign-'+this.tableId).find('option:selected').val();
 		}
-		
+
 		if(!data.right_value){
 			data.right_value = $j('#right-assign-'+this.tableId).find('option:selected').val();
 		}
-		
+
 		if(!data.left_text){
 			data.left_text = $j('#left-assign-'+this.tableId + ' option[value="'+data.left_value+'"]').text();
 		}
-		
+
 		if(!data.right_text){
 			data.right_text = $j('#right-assign-'+this.tableId + ' option[value="'+data.right_value+'"]').text();
 		}
-		
-		
-		
+
+
+
 		if((!data.left_value) || (!data.right_value)){
 			return;
 		}
-		
+
 		this.nextID +=1;
-		
+
 	    var cellLeft = $j('<td />', {
 	        'text': data.left_text
 	    });
 	    var cellRight = $j('<td />', {
 	        'text': data.right_text
 	    });
-	    
+
 	    var cellDelete = $j('<button />', {
 	        'class':'scalable delete add-widget position-delete',
 	        'type' : 'button',
 	        'onclick' : 'assign_'+ this.tableId +'.deleteRow('+ this.nextID +')'
 	    });
 	    cellDelete.append($j('<span />'));
-	    
+
 	    var inputLeft = $j('<input />', {
 	        'id'   : 'left_value-' + this.tableId + '-' + this.nextID,
 	        'name' : this.name +  '['+ this.nextID +'][left_value]',
@@ -76,8 +84,8 @@ function assignTable(tableId,name)
 	        'value': data.db_id,
 	        'type' : this.inputtype
 	    });
-	    
-	    
+
+
 	    cellRight.append(inputLeft).append(inputRigth).append(inputDelete).append(inputId);
 
 	    var cellFunction = $j('<td />', {
@@ -94,7 +102,5 @@ function assignTable(tableId,name)
 	    newRow.append(cellLeft).append(cellRight).append(cellDelete);
 
 	    $j('#table-' + this.tableId + ' tbody').append(newRow);
-
 	}
 }
-
