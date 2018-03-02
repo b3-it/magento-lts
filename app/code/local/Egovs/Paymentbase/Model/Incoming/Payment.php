a42ecafe97a5b257c156bdcdfae2bc25f7159269
Frank Rochlitzer <f.rochlitzer@b3-it.de>
2018-03-02 12:28:20 +0100

    Alle :: #3003 ePayBL-Capture-Date für alle Zahlverfahren
    * fix UTC Date
    * fix only add new entry on > 0.0

--- app/code/local/Egovs/Paymentbase/Model/Incoming/Payment.php
+++ app/code/local/Egovs/Paymentbase/Model/Incoming/Payment.php
@@ -6,7 +6,7 @@
  * @package		Egovs_Paymentbase
  * @name       	Egovs_Paymentbase_Model_Incoming_Payment
  * @author 		Holger Kögel <h.koegel@b3-it.de>
- * @copyright  	Copyright (c) 2017 B3 It Systeme GmbH - http://www.b3-it.de
+ * @copyright  	Copyright (c) 2018 B3 It Systeme GmbH - http://www.b3-it.de
  * @license		http://sid.sachsen.de OpenSource@SID.SACHSEN.DE
  */
 class Egovs_Paymentbase_Model_Incoming_Payment extends Mage_Core_Model_Abstract
@@ -17,8 +17,8 @@ class Egovs_Paymentbase_Model_Incoming_Payment extends Mage_Core_Model_Abstract
         $this->_init('paymentbase/incoming_payment');
     }
 
-    public function saveIncomingPayment($order_id,$base_amount,$amount)
+    public function saveIncomingPayment($order_id, $base_amount, $amount)
     {
-        $this->getResource()->saveIncomingPayment($this,$order_id,$base_amount,$amount);
+        $this->getResource()->saveIncomingPayment($this, $order_id, $base_amount, $amount);
     }
 }
--- app/code/local/Egovs/Paymentbase/Model/Mysql4/Incoming/Payment.php
+++ app/code/local/Egovs/Paymentbase/Model/Mysql4/Incoming/Payment.php
@@ -5,9 +5,10 @@
  * @category	Egovs
  * @package		Egovs_Paymentbase
  * @name       	Egovs_Paymentbase_Model_Mysql4_Incoming_Payment
-  * @author 		Holger Kögel <h.koegel@b3-it.de>
-  * @copyright  	Copyright (c) 2017 B3 It Systeme GmbH - http://www.b3-it.de
-  * @license		http://sid.sachsen.de OpenSource@SID.SACHSEN.DE
+ * @author 		Holger Kögel <h.koegel@b3-it.de>
+ * @author      Frank Rochlitzer <f.rochlitzer@b3-it.de>
+ * @copyright  	Copyright (c) 2018 B3 It Systeme GmbH - http://www.b3-it.de
+ * @license		http://sid.sachsen.de OpenSource@SID.SACHSEN.DE
  */
 class Egovs_Paymentbase_Model_Mysql4_Incoming_Payment extends Mage_Core_Model_Mysql4_Abstract
 {
@@ -23,15 +24,30 @@ class Egovs_Paymentbase_Model_Mysql4_Incoming_Payment extends Mage_Core_Model_My
         $this->_init('paymentbase/incoming_payment', 'id');
     }
 
-    public function saveIncomingPayment($object,$order_id,$base_amount, $amount )
+    public function saveIncomingPayment($object, $order_id, $base_amount, $amount )
     {
         $order_id = intval($order_id);
         $amount = floatval($amount);
         $base_amount = floatval($base_amount);
-        //insert into egovs_paymentbase_incoming_payment (order_id, total_paid, base_total_paid, epaybl_capture_date)
-       // (select 1,  sum(total_paid) - 3, sum(base_total_paid) - 3 , now() from egovs_paymentbase_incoming_payment where order_id = 1)
+        /*
+         * set @base_amount = 10.52;
+         * set @amount = @base_amount;
+         * set @order_id = 61;
+         * INSERT INTO magento19.egovs_paymentbase_incoming_payment (order_id, total_paid, base_total_paid, paid, base_paid,  epaybl_capture_date)
+         * (
+         *     SELECT @order_id, @amount, @base_amount, IFNULL(@amount - sum(paid),@amount), IFNULL(@base_amount - sum(base_paid),@base_amount) , UTC_TIMESTAMP()
+         *     FROM magento19.egovs_paymentbase_incoming_payment as t
+         *     WHERE order_id = @order_id
+         *     HAVING (IFNULL(@base_amount - sum(base_paid), @base_amount) > 0.00)
+         * )
+         */
         $sql = "INSERT INTO {$this->getMainTable()} (order_id, total_paid, base_total_paid, paid, base_paid,  epaybl_capture_date) ";
-        $sql .= "(SELECT {$order_id}, {$amount},{$base_amount}, IFNULL({$amount} - sum(paid),{$amount}), IFNULL({$base_amount} - sum(base_paid),{$base_amount}) , now() FROM {$this->getMainTable()} WHERE order_id = {$order_id})";
+        $sql .= "(";
+        $sql .= "SELECT {$order_id}, {$amount}, {$base_amount}, IFNULL({$amount} - sum(paid), {$amount}), IFNULL({$base_amount} - sum(base_paid), {$base_amount}) , UTC_TIMESTAMP()";
+        $sql .= " FROM {$this->getMainTable()}";
+        $sql .= " WHERE order_id = {$order_id}";
+        $sql .= " HAVING (IFNULL({$base_amount} - sum(base_paid), {$base_amount}) > 0.00)";
+        $sql .= ")";
         $this->_getWriteAdapter()->query($sql);
     }
 }
