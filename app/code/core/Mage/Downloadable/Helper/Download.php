0bc93ae68b4a0c8db3f9716d7aa8fecf2920ca62
Frank Rochlitzer <f.rochlitzer@b3-it.de>
2018-03-12 13:55:13 +0100

    Log file name if not exist

--- app/code/core/Mage/Downloadable/Helper/Download.php
+++ app/code/core/Mage/Downloadable/Helper/Download.php
@@ -180,7 +180,7 @@ class Mage_Downloadable_Helper_Download extends Mage_Core_Helper_Abstract
                 }
                 $this->_handle->open(array('path'=>Mage::getBaseDir('var')));
                 if (!$this->_handle->fileExists($this->_resourceFile, true)) {
-                    Mage::throwException(Mage::helper('downloadable')->__('The file does not exist.'));
+                    Mage::throwException(Mage::helper('downloadable')->__('The file %s does not exist.', $this->_resourceFile));
                 }
                 $this->_handle->streamOpen($this->_resourceFile, 'r');
             }
