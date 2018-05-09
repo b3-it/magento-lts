6c865024cbcb25e0da068926ecb94796b1b06d00
Frank Rochlitzer <f.rochlitzer@b3-it.de>
2018-05-09 11:16:24 +0200

    ~ BKG :: sql check fixed

--- app/code/local/Bkg/VirtualGeo/sql/bkg_virtualgeo_setup/upgrade-0.1.6.0-0.1.7.0.php
+++ app/code/local/Bkg/VirtualGeo/sql/bkg_virtualgeo_setup/upgrade-0.1.6.0-0.1.7.0.php
@@ -6,21 +6,14 @@ $installer = $this;
 $installer->startSetup();
 
 
-$tables = array();
+$table = 'virtualgeo/components_structure';
 
-$tables[] = 'virtualgeo/components_structure';
-
-
-foreach ($tables as $table)
+if ($installer->tableExists($installer->getTable($table.'_entity'))
+        && !$installer->getConnection()->tableColumnExists($installer->getTable($table.'_entity'), 'layer_naming_rule'))
 {
-	if ($installer->tableExists($installer->getTable($table.'_entity'))
-			&& !$installer->getConnection()->tableColumnExists($installer->getTable($table.'_product'), 'layer_naming_rule'))
-	{
-		$installer->run("ALTER TABLE {$installer->getTable($table.'_entity')}
-		ADD layer_naming_rule varchar(255) default '{{product_code}}_{{crs_code}}_{{structure_code}}'
-		");
-	}
+    $installer->run("ALTER TABLE {$installer->getTable($table.'_entity')}
+      ADD layer_naming_rule varchar(255) default '{{product_code}}_{{crs_code}}_{{structure_code}}'
+    ");
 }
-
 
 $installer->endSetup();
