bc3b026b3debd9117d9c64e5d376bf565603298f
Hans Mackowiak <h.mackowiak@b3-it.de>
2018-05-14 14:38:43 +0200

    * DWD :: mache Map URL secure wenn Request auch secure ist

--- app/code/local/Dwd/Stationen/Block/Catalog/Product/View/Map.php
+++ app/code/local/Dwd/Stationen/Block/Catalog/Product/View/Map.php
@@ -1,13 +1,17 @@
 <?php
 class Dwd_Stationen_Block_Catalog_Product_View_Map extends Dwd_Stationen_Block_Catalog_Product_View_Abstract
 {
-	public function getRssUrl()
-	{
-		return $this->getUrl('fstationen/liste/rss',array('product_id'=>$this->getProduct()->getId()));
-	}
+    public function getRssUrl()
+    {
+        return $this->getUrl('fstationen/liste/rss',array(
+            'product_id' => $this->getProduct()->getId(),
+            '_secure' => $this->getRequest()->isSecure()));
+    }
 
-	public function getGeoJsonUrl() {
-		return $this->getUrl('fstationen/liste/json',array('product_id'=>$this->getProduct()->getId()));
-	}
+    public function getGeoJsonUrl() {
+        return $this->getUrl('fstationen/liste/json',array(
+            'product_id' => $this->getProduct()->getId(),
+            '_secure' => $this->getRequest()->isSecure()));
+    }
 
 }
