780499c70eee56299077cb631921afaa85ffa663
Frank Rochlitzer <f.rochlitzer@b3-it.de>
2018-05-25 13:24:30 +0200

    * GKA :: VirtualPayId : Do some error handling

--- app/code/local/Gka/VirtualPayId/Block/Catalog/Product/View/Type.php
+++ app/code/local/Gka/VirtualPayId/Block/Catalog/Product/View/Type.php
@@ -105,7 +105,7 @@ class Gka_VirtualPayId_Block_Catalog_Product_View_Type extends Mage_Catalog_Bloc
 			$txt[] = '</select>';
 
 			if (!$_clientsAvailable) {
-                $this->getMessagesBlock()->addNotice($this->__("No valid ePayBL Client configuration available"));
+			    Mage::getSingleton('catalog/session')->addNotice($this->__("No valid ePayBL Client configuration available"));
             }
 			 
 			return implode(' ',$txt);
