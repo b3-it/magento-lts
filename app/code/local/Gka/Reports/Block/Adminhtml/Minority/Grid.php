127a70b939faf2f7e1e4609bcf93685ffcc6884e
Holger Kögel B3 IT <h.koegel@b3-it.de>
2018-07-09 17:07:16 +0200

    ~gka::reports Fremdanteil

---
+++ app/code/local/Gka/Reports/Block/Adminhtml/Minority/Grid.php
@@ -0,0 +1,171 @@
+<?php
+ /**
+  *
+  * @category   	Gka Reports
+  * @package    	Gka_Reports
+  * @name       	Gka_Reports_Block_Adminhtml_Minority_Grid
+  * @author 		Holger Kögel <h.koegel@b3-it.de>
+  * @copyright  	Copyright (c) 2017 B3 It Systeme GmbH - http://www.b3-it.de
+  * @license		http://sid.sachsen.de OpenSource@SID.SACHSEN.DE
+  */
+class Gka_Reports_Block_Adminhtml_Minority_Grid extends Mage_Adminhtml_Block_Widget_Grid
+{
+  public function __construct()
+  {
+      parent::__construct();
+      $this->setId('minorityBEGrid');
+      //$this->setDefaultSort('increment_id');
+      //$this->setDefaultDir('ASC');
+      $this->setSaveParametersInSession(true);
+      $this->setDefaultLimit(100);
+      $this->setUseAjax(true);
+
+
+      $from = date("Y-m-d", strtotime('-1 day'));
+      $to = date("Y-m-d", strtotime('-0 day'));
+      $locale = Mage::app()->getLocale()->getLocaleCode();
+
+      $this->setDefaultFilter(array(
+          "created_at"=>array(
+              'from'=> new Zend_Date($from, null, $locale),
+              'to'=> new Zend_Date($to, null, $locale),
+              'locale' => $locale,
+              'orig_to' => Mage::helper('core')->formatDate($to),
+              'orig_from' => Mage::helper('core')->formatDate($from),
+              'datetime' => true
+          )
+
+      ));
+
+  }
+
+    protected function _prepareCollection() {
+        $collection = Mage::getModel('sales/order_item')->getCollection();
+
+        $eav = Mage::getResourceModel('eav/entity_attribute');
+        ;
+
+        $collection->getSelect()
+            ->join(array('torder' => $collection->getTable('sales/order_grid')), 'torder.entity_id = main_table.order_id',array('status','order_currency_code'))
+            ->joinleft(array('product'=>$collection->getTable("catalog/product")."_varchar"),'product.entity_id=main_table.product_id AND product.attribute_id='.intval($eav->getIdByCode('catalog_product','minority_interest')),array('minority_interest'=>'value') )
+            ->joinleft(array('t1'=>$collection->getTable('customer/entity').'_varchar'), 'torder.customer_id=t1.entity_id AND t1.attribute_id = '.intval($eav->getIdByCode('customer','company')),array('company'=>'value') )
+            ->columns(array('sum_total'=>'sum(base_row_total)'))
+            ->group(array('main_table.store_id', 'customer_id', 'product_id'))
+            ->where('parent_item_id is not null')
+            ->where( new Zend_Db_Expr("(torder.status='complete') OR (torder.status='complete')"))
+        //    ->where('main_table.store_id = ' . intval(Mage::app()->getStore()->getId()))
+        ;
+
+        //die($collection->getSelect()->__toString());
+
+        $this->setCollection($collection);
+
+        return parent::_prepareCollection();
+    }
+
+    protected function _prepareColumns() {
+
+        $this->addColumn('created_at', array(
+            'header'    => Mage::helper('sales')->__('Created At'),
+            'align'     =>'left',
+            'index'     => 'created_at',
+            'filter_index' => 'main_table.created_at',
+            'width'		=> '150',
+            'type'	=> 'date',
+        ));
+
+
+        $this->addColumn('store_id', array(
+            'header'    => Mage::helper('sales')->__('Store'),
+            'index'     => 'store_id',
+            'type'      => 'store',
+            'width'     => '400px',
+            'store_view'=> true,
+            'display_deleted' => true,
+        ));
+
+        $this->addColumn('billing_name', array(
+            'header' => Mage::helper('sales')->__('Operator'),
+            'index' => 'company',
+            'filter_index' => 't1.value'
+        ));
+
+        $this->addColumn('sku', array(
+            'header' => Mage::helper('sales')->__('sku'),
+            'index' => 'sku',
+            //'filter_index' => 't1.value'
+        ));
+
+        $this->addColumn('sum_total', array(
+            'header' => Mage::helper('sales')->__('Amount'),
+            'index' => 'sum_total',
+            'type' => 'currency',
+            'currency' => 'order_currency_code',
+            'total' => 'sum',
+        ));
+
+        $yn = Mage::getSingleton('adminhtml/system_config_source_yesno')->toOptionArray();
+        $yesno = array();
+        foreach ($yn as $n)
+        {
+            $yesno[$n['value']] = $n['label'];
+        }
+
+        $this->addColumn('minority_interest', array(
+            'header' => Mage::helper('sales')->__('Minority Interest'),
+            'index' => 'minority_interest',
+            'type'  => 'options',
+            'width' => '70px',
+            'filter_index' => 'product.value',
+            'options' => $yesno,
+        ));
+
+        $this->addExportType('*/*/exportCsv', Mage::helper('gka_reports')->__('CSV'));
+        $this->addExportType('*/*/exportXml', Mage::helper('gka_reports')->__('XML'));
+        $this->addExportType('*/*/exportExcel', Mage::helper('gka_reports')->__('XML (Excel)'));
+        $this->setCountTotals(true);
+        return parent::_prepareColumns();
+    }
+
+
+    public function getTotals() {
+        return $this->_varTotals;
+    }
+
+
+    protected function _afterLoadCollection() {
+        $data = array();
+        foreach ($this->getCollection() as $item) {
+            foreach ($this->getColumns() as $col) {
+                if ($col->getTotal()) {
+                    if (!isset($data[$col->getId()])) {
+                        $data[$col->getId()] = $item->getData($col->getIndex());
+                    } else {
+                        $data[$col->getId()] += $item->getData($col->getIndex());
+                    }
+                }
+            }
+        }
+
+        $helper = Mage::helper('core');
+        foreach ($data as $key => $value) {
+            $data[$key] = $helper->currency($value, true, false);
+        }
+
+
+        $this->setTotals(new Varien_Object($data));
+    }
+
+
+	public function getGridUrl($params = array())
+    {
+    	if (!isset($params['_current'])) {
+    		$params['_current'] = true;
+    	}
+    	return $this->getUrl('*/*/grid', $params);
+
+    }
+
+
+
+}
---
+++ app/code/local/Gka/Reports/sql/gkareports_setup/upgrade-0.1.0-0.1.1.php
@@ -0,0 +1,50 @@
+<?php
+
+$installer = $this;
+
+$installer->startSetup();
+
+
+$installer->addAttribute('catalog_product', 'minority_interest', array(
+    'label' => 'Minority Interest',
+    'input' => 'select',
+    'type' => 'varchar',
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+    'visible' => true,
+    'required' => true,
+    'is_user_defined' => true,
+    'searchable' => false,
+    'comparable' => false,
+    'visible_on_front' => false,
+    'visible_in_advanced_search' => false,
+    'source'  => 'eav/entity_attribute_source_boolean',
+    'default' => '0',
+    //'option' => $option,
+    'group' => 'General',
+));
+
+
+/*
+$columnName = 'minority_interest';
+if (!$installer->getConnection()->tableColumnExists($installer->getTable('sales/quote_item'), $columnName)) {
+	$installer->getConnection()->addColumn($installer->getTable('sales/quote_item'), $columnName, array(
+			'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+			'nullable' => true,
+			'length' => 48,
+			'default' => null,
+            'comment' => $columnName
+	));
+}
+
+if (!$installer->getConnection()->tableColumnExists($installer->getTable('sales/order_item'), $columnName)) {
+	$installer->getConnection()->addColumn($installer->getTable('sales/order_item'), $columnName, array(
+			'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+			'nullable' => true,
+			'length' => 48,
+			'default' => null,
+            'comment' => $columnName
+	));
+}
+*/
+
+$installer->endSetup(); 
