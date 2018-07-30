afd7252476c4b65b9cb026702e4634ac3a7b5c24
Frank Rochlitzer <f.rochlitzer@b3-it.de>
2018-07-30 10:43:55 +0200

    * Alle :: #3115 ZV_AM-1144 Erhöhung der Längenbegrenzung für das Feld "Buchungstext"

--- app/code/local/Egovs/Paymentbase/Model/Webservice/Types/Buchung.php
+++ app/code/local/Egovs/Paymentbase/Model/Webservice/Types/Buchung.php
@@ -4,19 +4,21 @@
  * 
  * @property string haushaltsstelle Analog F15 Buchungszeile. Dient zur Steuerung der Sachkontenverbuchung im HKR Verfahren (10 Zeichen)
  * @property string objektnummer Analog F15 Buchungszeile. Dient zur Steuerung der Sachkontenverbuchung im HKR Verfahren. ((10 Zeichen) Optional)
- * @property string buchungstext Pflicht Der Text darf nur aus den Zeichen AZ0-9,.:/+& -*$%ÄÖÜß bestehen (21 Zeichen)
+ * @property string buchungstext Pflicht Der Text darf nur aus den Zeichen AZ0-9,.:/+& -*$%ÄÖÜß bestehen (120 Zeichen)
  * @property float betrag Betrag zur Titelverbuchung. Die Summe aller Beträge im Array muss gleich dem Betrag der Buchungsliste sein. ((15,2) Optional)
  * @property string belegNr Je nach Mandantenkonfiguration wird im ePayment System die Belegnummer ermittelt und in der Ergebnisstruktur zurückgeliefert. Wird die Belegnummer mitgeliefert, muss sie aus 8 Ziffern bestehen (ggf. mit führenden '0' auffüllen) und eindeutig im Belegnummernkreis des Bewirtschafters sein. (8 Zeichen Optional)
  * @property href In diesem Feld kann eine Buchungsreferenz von der Fachanwendung entgegengenommen werden. Diese wird als sogenannter Rucksackdatensatz an das ZÜV übermittelt. Weitere Details siehe Change Request 58. ((100 Zeichen) Optional)
  * 
  * @category	Egovs
  * @package		Egovs_Paymentbase
- * @author 		Frank Rochlitzer <f.rochlitzer@edv-beratung-hempel.de>
- * @copyright	Copyright (c) 2012 EDV Beratung Hempel
+ * @author 		Frank Rochlitzer <f.rochlitzer@b3-it.de>
+ * @copyright	Copyright (c) 2012 - 2018 B3 IT Systeme GmbH - https://www.b3-it.de
  * @license		http://sid.sachsen.de OpenSource@SID.SACHSEN.DE
  */
 class Egovs_Paymentbase_Model_Webservice_Types_Buchung extends Egovs_Paymentbase_Model_Webservice_Types_Abstract
 {
+    const LENGTH_BUCHUNGSTEXT = 120;
+
 	/**
 	 * Konstruktor
 	 * 
@@ -68,7 +70,7 @@ class Egovs_Paymentbase_Model_Webservice_Types_Buchung extends Egovs_Paymentbase
 	 *  <li>belegNr = 8</li>
 	 *  <li>haushaltsstelle = 10</li>
 	 *  <li>objektnummer = 10</li>
-	 *  <li>buchungstext = 21</li>
+	 *  <li>buchungstext = 120</li>
 	 *  <li>HREF = 255</li>
 	 *  <li><strong>default</strong> = 1000</li>
 	 * </ul>
@@ -87,8 +89,9 @@ class Egovs_Paymentbase_Model_Webservice_Types_Buchung extends Egovs_Paymentbase
 			case 'haushaltsstelle':
 			case 'objektnummer':
 				$length = 10;
+				break;
 			case 'buchungstext':
-				$length = 21;
+				$length = self::LENGTH_BUCHUNGSTEXT;
 				break;
 			case 'href':
 				if ($ml = Mage::getStoreConfig('payment_services/paymentbase/href_max_length')) {
