2ff5fabcf30505e2b76aa1dd393678c767a29368
René Mütterlein <r.muetterlein@b3-it.de>
2018-08-22 14:16:19 +0200

    +SMWK :: Design : Design nach Markenhandbuchvorgabe in Dunkelblau

---
+++ skin/frontend/egov/smwk_zeuss/scss/config.rb
@@ -0,0 +1,13 @@
+# note: this should never truly be refernced since we are using relative assets
+http_path = "/skin/frontend/egov/stala/"
+css_dir = "../css"
+sass_dir = "../scss"
+images_dir = "../images"
+javascripts_dir = "../../default/js"
+font_dir = "../../default/font"
+default_dir = "../../default"
+relative_assets = true
+sass_options = {:unix_newlines => true}
+
+output_style = :compressed
+environment = :production
