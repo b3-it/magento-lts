5a02d8bb7b2d0a816f9a2db67299d7b31390475c
Holger Kögel B3 It <h.koegel@b3-it.de>
2018-10-02 16:43:30 +0200

    ~ SID:: Dateien hinzu

---
+++ app/code/local/Egovs/B2b/Helper/Data.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * <Namespace> <Module>
+ *
+ *
+ * @category   	<Namespace>
+ * @package    	<Namespace>_<Module>
+ * @name       	<Namespace>_<Module>_Helper_Data
+ * @author 		Holger Kögel <h.koegel@b3-it.de>
+ * @copyright  	Copyright (c) 2015 B3 It Systeme GmbH - http://www.b3-it.de
+ * @license		http://sid.sachsen.de OpenSource@SID.SACHSEN.DE
+ */
+
+ /**
+  *
+  * @category   	Egovs B2b
+  * @package    	Egovs_B2b
+  * @name       	Egovs_B2b_Helper_Data
+  * @author 		Holger Kögel <h.koegel@b3-it.de>
+  * @copyright  	Copyright (c) 2017 B3 It Systeme GmbH - http://www.b3-it.de
+  * @license		http://sid.sachsen.de OpenSource@SID.SACHSEN.DE
+  */
+class Egovs_B2b_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
