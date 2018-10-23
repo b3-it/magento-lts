218f0b6e99538e123223bf70f2913f5668bdeff0
René Mütterlein <r.muetterlein@b3-it.de>
2018-10-23 14:56:17 +0200

    *Alle :: Captcha : Layout begonnen (funktioniert nicht)

---
+++ app/code/local/Egovs/Captcha/Block/Googlecaptcha.php
@@ -0,0 +1,6 @@
+<?php
+
+class Egovs_Captcha_Block_Googlecaptcha extends Mage_Captcha_Block_Captcha_Zend
+{
+    protected $_template = 'egovs/captcha/googlecaptcha.phtml';
+}
---
+++ app/code/local/Egovs/Captcha/Block/Iconcaptcha.php
@@ -0,0 +1,6 @@
+<?php
+
+class Egovs_Captcha_Block_Iconcaptcha extends Mage_Captcha_Block_Captcha_Zend
+{
+    protected $_template = 'egovs/captcha/iconcaptcha.phtml';
+}
