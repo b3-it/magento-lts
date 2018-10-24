d0d541dab62d1a2b2e28f4d5797c98bd023cd5bf
René Mütterlein <r.muetterlein@b3-it.de>
2018-10-24 10:06:49 +0200

    *Alle :: Captcha : Merge

--- app/code/local/Egovs/Captcha/Block/Iconcaptcha.php
+++ app/code/local/Egovs/Captcha/Block/Iconcaptcha.php
@@ -1,6 +1,25 @@
 <?php
 
-class Egovs_Captcha_Block_Iconcaptcha extends Mage_Captcha_Block_Captcha_Zend
+class Egovs_Captcha_Block_IconCaptcha extends Mage_Captcha_Block_Captcha_Zend
 {
     protected $_template = 'egovs/captcha/iconcaptcha.phtml';
+
+    /**
+     * Internal constructor, that is called from real constructor
+     *
+     */
+    protected function _construct()
+    {
+        parent::_construct();
+    }
+
+    /**
+     * Returns captcha model
+     *
+     * @return Mage_Captcha_Model_Abstract
+     */
+    public function getCaptchaModel()
+    {
+        return Mage::helper('egovscaptcha')->getCaptcha($this->getFormId());
+    }
 }
