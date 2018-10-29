7812113ada05077957598a9461ec1f08c4a21601
Frank Rochlitzer <f.rochlitzer@b3-it.de>
2018-10-29 12:53:10 +0100

    *Alle :: Captcha : case-sensitivy problem

--- app/code/local/Egovs/Captcha/Block/Googlev2Captcha.php
+++ app/code/local/Egovs/Captcha/Block/Googlev2Captcha.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * Class Egovs_Captcha_Block_Googlev2Captcha
+ *
+ * @category  Egovs
+ * @package   Egovs_Captcha
+ * @author    Frank Rochlitzer <f.rochlitzer@b3-it.de>
+ * @copyright Copyright (c) 2018 B3 IT Systeme GmbH - https://www.b3-it.de
+ * @license   http://sid.sachsen.de OpenSource@SID.SACHSEN.DE
+ */
 class Egovs_Captcha_Block_Googlev2Captcha extends Mage_Captcha_Block_Captcha_Zend
 {
     protected $_template = 'egovs/captcha/googlecaptcha.phtml';
--- app/code/local/Egovs/Captcha/Block/IconCaptcha.php
+++ app/code/local/Egovs/Captcha/Block/IconCaptcha.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * Class Egovs_Captcha_Block_IconCaptcha
+ *
+ * @category  Egovs
+ * @package   Egovs_Captcha
+ * @author    Frank Rochlitzer <f.rochlitzer@b3-it.de>
+ * @copyright Copyright (c) 2018 B3 IT Systeme GmbH - https://www.b3-it.de
+ * @license   http://sid.sachsen.de OpenSource@SID.SACHSEN.DE
+ */
 class Egovs_Captcha_Block_IconCaptcha extends Mage_Captcha_Block_Captcha_Zend
 {
     protected $_template = 'egovs/captcha/iconcaptcha.phtml';
