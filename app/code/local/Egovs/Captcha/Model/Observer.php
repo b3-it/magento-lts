7989e474d60125f90652ed5638c0ed2e5cd62768
Frank Rochlitzer <f.rochlitzer@b3-it.de>
2018-11-29 16:56:03 +0100

    * SUPEE-10975 ePayBL Webshop Module

--- app/code/local/Egovs/Captcha/Model/Observer.php
+++ app/code/local/Egovs/Captcha/Model/Observer.php
@@ -240,4 +240,67 @@ class Egovs_Captcha_Model_Observer extends Mage_Captcha_Model_Observer
         }
         return $this;
     }
+
+    /**
+     * Check Captcha On Share Wishlist Page
+     *
+     * @param Varien_Event_Observer $observer
+     * @return Mage_Captcha_Model_Observer
+     */
+    public function checkWishlistSharing($observer)
+    {
+        $formId = 'wishlist_sharing';
+        $captchaModel = Mage::helper('egovscaptcha')->getCaptcha($formId);
+        if ($captchaModel->isRequired()) {
+            $controller = $observer->getControllerAction();
+            $request = $controller->getRequest();
+            if ($captchaModel instanceof Mage_Captcha_Model_Zend) {
+                $data = $this->_getCaptchaString($request, $formId);
+            } else {
+                $data = $captchaModel->getRequiredPostFields($controller);
+            }
+            if (!$captchaModel->isCorrect($data)) {
+                Mage::getSingleton('wishlist/session')->addError(Mage::helper('captcha')->__('Incorrect CAPTCHA.'));
+                $controller->setFlag('', Mage_Core_Controller_Varien_Action::FLAG_NO_DISPATCH, true);
+                Mage::getSingleton('wishlist/session')->setSharingForm($request->getPost());
+                $wishlistId = (int)$request->getParam('wishlist_id');
+                $controller->getResponse()
+                    ->setRedirect(Mage::getUrl('wishlist/index/share/wishlist_id/' . $wishlistId));
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * Check Captcha On Email Product To A Friend Page
+     *
+     * @param Varien_Event_Observer $observer
+     * @return Mage_Captcha_Model_Observer
+     */
+    public function checkSendfriendSend($observer)
+    {
+        $formId = 'sendfriend_send';
+        $captchaModel = Mage::helper('egovscaptcha')->getCaptcha($formId);
+        if ($captchaModel->isRequired()) {
+            $controller = $observer->getControllerAction();
+            $request = $controller->getRequest();
+            if ($captchaModel instanceof Mage_Captcha_Model_Zend) {
+                $data = $this->_getCaptchaString($request, $formId);
+            } else {
+                $data = $captchaModel->getRequiredPostFields($controller);
+            }
+            if (!$captchaModel->isCorrect($data)) {
+                Mage::getSingleton('catalog/session')->addError(Mage::helper('captcha')->__('Incorrect CAPTCHA.'));
+                $controller->setFlag('', Mage_Core_Controller_Varien_Action::FLAG_NO_DISPATCH, true);
+                Mage::getSingleton('catalog/session')->setFormData($request->getPost());
+                $id = (int)$request->getParam('id');
+                $catId = $request->getParam('cat_id');
+                if (null !== $catId) {
+                    $id .= '/cat_id/' . (int)$catId;
+                }
+                $controller->getResponse()->setRedirect(Mage::getUrl('*/*/send/id/' . $id));
+            }
+        }
+        return $this;
+    }
 }
