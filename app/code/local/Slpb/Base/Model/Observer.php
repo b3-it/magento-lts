39dc6d47773da8ea2c364a94bd502277bba2a26b
Frank Rochlitzer <f.rochlitzer@b3-it.de>
2019-03-20 16:36:53 +0100

    * Slpb :: AM-1204 Sort by sku while creating order in backend

--- app/code/local/Slpb/Base/Model/Observer.php
+++ app/code/local/Slpb/Base/Model/Observer.php
@@ -19,4 +19,44 @@ class Slpb_Base_Model_Observer
 
         $collection->setOrder('sku', Mage_Sales_Model_Resource_Order_Item_Collection::SORT_ORDER_ASC);
     }
+
+    public function onSalesQuoteItemCollectionLoadBefore($observer) {
+        $collection = $observer->getQuoteItemCollection();
+        if (!($collection instanceof Mage_Sales_Model_Resource_Quote_Item_Collection)) {
+            return;
+        }
+
+        $collection->setOrder('sku', Mage_Sales_Model_Resource_Quote_Item_Collection::SORT_ORDER_ASC);
+    }
+
+    public function onAdminhtmlSalesOrderCreateProcessData ($observer) {
+        /** @var \Mage_Adminhtml_Model_Sales_Order_Create $orderCreateModel */
+        $orderCreateModel = $observer->getOrderCreateModel();
+        if (!$orderCreateModel instanceof Mage_Adminhtml_Model_Sales_Order_Create) {
+            return;
+        }
+
+        //Resort items by SKU after add to cart
+        //Quote is already loaded, so we do it this way
+        $itemsCollection = $orderCreateModel->getQuote()->getItemsCollection();
+        $items = $itemsCollection->getItems();
+        uasort($items, array(__CLASS__, 'compare'));
+        foreach ($items as $k => $item) {
+            $itemsCollection->removeItemByKey($k);
+            $itemsCollection->addItem($item);
+        }
+    }
+
+    public static function compare($a, $b) {
+        $aSku = $a->getSku();
+        $bSku = $b->getSku();
+        if ($aSku === $bSku) {
+            return 0;
+        }
+        if (is_numeric($aSku) && is_numeric($bSku)) {
+            return ($aSku > $bSku) ? +1 : -1;
+        }
+
+        return strcmp($aSku, $bSku);
+    }
 }
