ef83345a0bbde2c99602a4aa317546e190df2292
René Mütterlein <r.muetterlein@b3-it.de>
2019-03-29 14:28:59 +0100

    *Alle :: Design : NiceScroll auf Touche-Geräten abgeschaltet

--- js/egovs/jquery.nicescroll.init.js
+++ js/egovs/jquery.nicescroll.init.js
@@ -1,39 +1,40 @@
 
-var color_element  = '';
-var color_proberty = '';
-var scroll_element = '';
+let color_element  = "";
+let color_proberty = "";
+let scroll_element = "";
+
 function updateScrollbar(localScrollElement)
 {
-    if ( detectIE() != false ) {
+    if ( detectIE() != false && is_touch_device() != false ) {
         return;
     }
     $j(localScrollElement).niceScroll({
-        'cursorcolor'       : $j(color_element).css(color_proberty),
-        'cursorwidth'       : '15px',
-        'cursorborderradius': '3px',
-        'horizrailenabled'  : false,
-        autohidemode:  true,
-        bouncescroll:  false,
-        smoothscroll:  true,
-        touchbehavior: false,
-        zindex:        999
+        "cursorcolor"       : $j(color_element).css(color_proberty),
+        "cursorwidth"       : "15px",
+        "cursorborderradius": "3px",
+        "horizrailenabled"  : false,
+        "autohidemode"      : true,
+        "bouncescroll"      : false,
+        "smoothscroll"      : true,
+        "touchbehavior"     : false,
+        "zindex"            : 999
     }).resize();
 }
 
 (function($){
     $(document).ready(function() {
-        if ( typeof(isDWD) !== 'undefined' ) {
-            var color_element  = '#top-row';
-            var color_proberty = 'background-color';
-            var scroll_element = 'body';
+        if ( typeof(isDWD) !== "undefined" ) {
+            color_element  = "#top-row";
+            color_proberty = "background-color";
+            scroll_element = "body";
         }
         else {
-            var color_element  = '.page-title h1';
-            var color_proberty = 'color';
-            var scroll_element = 'body';
+            color_element  = ".page-title h1";
+            color_proberty = "color";
+            scroll_element = "body";
         }
 
-        if ( detectIE() == false ) {
+        if ( detectIE() == false && is_touch_device() == false ) {
             updateScrollbar(scroll_element);
         }
     });
