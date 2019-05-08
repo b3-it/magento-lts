66f286c561e10ef85df70a1aacfc156457e285e9
René Mütterlein <r.muetterlein@b3-it.de>
2019-05-08 15:57:18 +0200

    *Alle :: JS : Polished

--- js/egovs/jquery.context.helper.popup.js
+++ js/egovs/jquery.context.helper.popup.js
@@ -1,48 +1,52 @@
-$j(document).ready(function(){
-    $j('#context-help').css('display', 'inline-block');
-});
+(function($j) {
+    let elementId = "#context-help-popup";
 
-/**
- * Popup für eine Anzeige vorbereiten
- */
-function resetPopup()
-{
-    $j(popupBodyId).html('');
-    hidePopup();
-}
-
-/**
- * Popup ausblenden
- */
-function hidePopup()
-{
-    $j('#context-help-popup').css({
-        'z-index': hideLevel,
-        'left'   : '-100000px'
+    $j(document).ready(function(){
+        $j("#context-help").css("display", "inline-block");
     });
-}
 
-/**
- * Popup anzeigen
- */
-function viewPopup()
-{
-    $j('#context-help-popup').css({
-        'z-index': showLevel,
-        'left'   : '20%'
-    });
-}
+    /**
+     * Popup ausblenden
+     */
+    function hidePopup()
+    {
+        $j(elementId).css({
+            "z-index": hideLevel,
+            "left"   : "-100000px"
+        });
+    }
 
-/**
- * Scrollbar innerhalb des sichtbaren Popup neu zeichnen
- */
-function updatePopupScrollBar()
-{
-    // Funktion in js/egovs/jquery.nicescroll.init.js
-    updateScrollbar(popupBodyId);
+    /**
+     * Popup anzeigen
+     */
+    function viewPopup()
+    {
+        $j(elementId).css({
+            "z-index": showLevel,
+            "left"   : "20%"
+        });
+    }
 
-    // Resize-Event der Content-Box
-    $j(popupBodyId).resize(function(){
-        $j(popupBodyId).getNiceScroll().resize();
-    });
-}
+    /**
+     * Popup für eine Anzeige vorbereiten
+     */
+    function resetPopup()
+    {
+        $j(popupBodyId).html("");
+        hidePopup();
+    }
+
+    /**
+     * Scrollbar innerhalb des sichtbaren Popup neu zeichnen
+     */
+    function updatePopupScrollBar()
+    {
+        // Funktion in js/egovs/jquery.nicescroll.init.js
+        updateScrollbar(popupBodyId);
+
+        // Resize-Event der Content-Box
+        $j(popupBodyId).resize(function(){
+            $j(popupBodyId).getNiceScroll().resize();
+        });
+    }
+})(jQuery);
