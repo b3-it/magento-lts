c320e548db4512438df071b076726a8525385d7a
René Mütterlein <r.muetterlein@b3-it.de>
2019-06-18 13:59:46 +0200

    *Alle :: BugFix : ResetPopup is not defined

--- js/egovs/jquery.context.helper.popup.js
+++ js/egovs/jquery.context.helper.popup.js
@@ -1,52 +1,50 @@
-(function($j) {
-    let elementId = "#context-help-popup";
+let elementId = "#context-help-popup";
 
-    $j(document).ready(function(){
-        $j("#context-help").css("display", "inline-block");
+/**
+ * Popup ausblenden
+ */
+function hidePopup()
+{
+    $j(elementId).css({
+        "z-index": hideLevel,
+        "left"   : "-100000px"
     });
+}
 
-    /**
-     * Popup ausblenden
-     */
-    function hidePopup()
-    {
-        $j(elementId).css({
-            "z-index": hideLevel,
-            "left"   : "-100000px"
-        });
-    }
+/**
+ * Popup anzeigen
+ */
+function viewPopup()
+{
+    $j(elementId).css({
+        "z-index": showLevel,
+        "left"   : "20%"
+    });
+}
 
-    /**
-     * Popup anzeigen
-     */
-    function viewPopup()
-    {
-        $j(elementId).css({
-            "z-index": showLevel,
-            "left"   : "20%"
-        });
-    }
+/**
+ * Popup für eine Anzeige vorbereiten
+ */
+function resetPopup()
+{
+    $j(popupBodyId).html("");
+    hidePopup();
+}
 
-    /**
-     * Popup für eine Anzeige vorbereiten
-     */
-    function resetPopup()
-    {
-        $j(popupBodyId).html("");
-        hidePopup();
-    }
+/**
+ * Scrollbar innerhalb des sichtbaren Popup neu zeichnen
+ */
+function updatePopupScrollBar()
+{
+    // Funktion in js/egovs/jquery.nicescroll.init.js
+    updateScrollbar(popupBodyId);
 
-    /**
-     * Scrollbar innerhalb des sichtbaren Popup neu zeichnen
-     */
-    function updatePopupScrollBar()
-    {
-        // Funktion in js/egovs/jquery.nicescroll.init.js
-        updateScrollbar(popupBodyId);
+    // Resize-Event der Content-Box
+    $j(popupBodyId).resize(function(){
+        $j(popupBodyId).getNiceScroll().resize();
+    });
+}
 
-        // Resize-Event der Content-Box
-        $j(popupBodyId).resize(function(){
-            $j(popupBodyId).getNiceScroll().resize();
-        });
-    }
-})(jQuery);
+$j(document).ready(function(){
+    $j("#context-help").css("display", "inline-block");
+});
