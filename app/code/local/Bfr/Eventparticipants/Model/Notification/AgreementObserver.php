bd68572c721ee175728384b0a419870c7a9176b7
h.matthes <h.matthes@b3-it.de>
2019-07-08 13:27:58 +0200

    ~ Bfr_EventParticipants :: added observer for checkout/after to write into table Notification_order

--- app/code/local/Bfr/Eventparticipants/Model/Notification/AgreementObserver.php
+++ app/code/local/Bfr/Eventparticipants/Model/Notification/AgreementObserver.php
@@ -12,32 +12,30 @@
 class Bfr_Eventparticipants_Model_Notification_AgreementObserver extends Mage_Core_Model_Abstract
 {
     /**
-     * Product gets added to the shopping cart
-     *
-     * @param $observer
+     * @param Varien_Event_Observer $observer
+     * @return Varien_Event_Observer
+     * @throws Exception
      */
-    public function AfterCartAddProduct($observer)
+    public function OnCheckoutSubmitAllAfter(Varien_Event_Observer $observer)
     {
+        /** @var Mage_Sales_Model_Quote $quote */
+        $quote = $observer->getQuote();
 
-    }
-
-    /**
-     * Product gets edited while in the shopping cart
-     *
-     * @param $observer
-     */
-    public function AfterCardEditProduct($observer)
-    {
-
-    }
-
-    /**
-     * Product gets removed from the shopping cart (kein event in magento?)
-     *
-     * @param $observer
-     */
-    public function AfterCartRemoveProduct($observer)
-    {
-
+        /** @var Mage_Sales_Model_Quote_Item $item */
+        foreach($quote->getAllVisibleItems() as $item){
+            if($item->getProductType() == Egovs_EventBundle_Model_Product_Type::TYPE_EVENTBUNDLE && $item->getBuyRequest()->getData('eventparticipants') === 'on'){
+                /** @var Bfr_Eventparticipants_Model_Notification_Order $notification */
+                $notification = Mage::getModel('bfr_eventparticipants/notification_order');
+                $notification->setEventId($item->getProductId());
+                $notification->setSignedAt('2000-1-1 12:00:00');
+                $notification->setHash('hash');
+                $notification->setOrderItemId($item->getId());
+                $notification->setStatus(0);
+                $notification->setCustomerId($item->getQuote()->getCustomerId());
+                $notification->setQuoteItemId($item->getQuoteId());
+                $notification->save();
+            }
+        }
+        return $observer;
     }
 }
--- app/code/local/Bfr/Eventparticipants/Model/Notification/Order.php
+++ app/code/local/Bfr/Eventparticipants/Model/Notification/Order.php
@@ -8,6 +8,25 @@
  * @copyright  	Copyright (c) 2017 B3 It Systeme GmbH - http://www.b3-it.de
  * @license		http://sid.sachsen.de OpenSource@SID.SACHSEN.DE
  */
+
+/**
+ *  @method int getId()
+ *  @method setId(int $value)
+ *  @method int getQuoteItemId()
+ *  @method setQuoteItemId(int $value)
+ *  @method int getOrderItemId()
+ *  @method setOrderItemId(int $value)
+ *  @method int getCustomerId()
+ *  @method setCustomerId(int $value)
+ *  @method string getHash()
+ *  @method setHash(string $value)
+ *  @method int getStatus()
+ *  @method setStatus(int $value)
+ *  @method int getEventId()
+ *  @method setEventId(int $value)
+ *  @method  getSignedAt()
+ *  @method setSignedAt( $value)
+ */
 class Bfr_Eventparticipants_Model_Notification_Order extends Mage_Core_Model_Abstract
 {
     public function _construct()
