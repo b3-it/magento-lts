b9da0b6918d65d214ecc12fb32340c990559dc14
h.matthes <h.matthes@b3-it.de>
2019-07-10 13:49:22 +0200

    ~ Bfr_EventParticipants :: controller save action for participationlist agreement

--- app/code/local/Bfr/Eventparticipants/controllers/Notification/OrderController.php
+++ app/code/local/Bfr/Eventparticipants/controllers/Notification/OrderController.php
@@ -1,50 +1,34 @@
 <?php
-  /**
-   *
-   * @category   	Bfr Eventparticipants
-   * @package    	Bfr_Eventparticipants
-   * @name        Bfr_Eventparticipants_Notification_OrderController
-   * @author 		Holger Kögel <h.koegel@b3-it.de>
-   * @copyright  	Copyright (c) 2017 B3 It Systeme GmbH - http://www.b3-it.de
-   * @license		http://sid.sachsen.de OpenSource@SID.SACHSEN.DE
-   */
+
+/**
+ *
+ * @category    Bfr Eventparticipants
+ * @package     Bfr_Eventparticipants
+ * @name        Bfr_Eventparticipants_Notification_OrderController
+ * @author      Holger Kögel <h.koegel@b3-it.de>
+ * @copyright   Copyright (c) 2017 B3 It Systeme GmbH - http://www.b3-it.de
+ * @license     http://sid.sachsen.de OpenSource@SID.SACHSEN.DE
+ */
 class Bfr_Eventparticipants_Notification_OrderController extends Mage_Core_Controller_Front_Action
 {
-    protected $_customer = null;
-
+    /**
+     * @throws Exception
+     */
     public function indexAction()
     {
-      $id     =  intval($this->getRequest()->getParam('id'));
-      $model  = Mage::getModel('bfr_eventparticipants/notification_order')->load($id);
+        $model = Mage::getModel('bfr_eventparticipants/notification_order');
+        $hash = $this->getRequest()->getParam('hash');
 
-      if ($model->getId() || $id == 0)
-      {
-        Mage::register('notificationorder_data', $model);
-      }else{
-        Mage::getSingleton('core/session')->addError($this->__('Internal Error'));
-        Mage::log('bfr_eventparticipants/notification_order not found ID:'.$id);
-        $this->_redirect('customer/account');
-        return;
-      }
-
-
-      $this->loadLayout();
-      $this->renderLayout();
-    }
-
-    public function preDispatch() {
-    	parent::preDispatch();
-
-    	if (!Mage::getSingleton('customer/session')->authenticate($this)) {
-    		$this->setFlag('', 'no-dispatch', true);
-    	}
-    }
-
-    protected function _getCustomer()
-    {
-        if($this->_customer == null){
-          $this->_customer = Mage::getSingleton('customer/session')->getCustomer();
+        if($hash != null ){
+            $model->load($hash, 'hash');
         }
-        return $this->_custommer;
+
+        if($model->hasData('id') && $model->getData('status') === '0'){
+            $model->setStatus(1);
+            $model->save();
+        }
+
+        $this->loadLayout();
+        $this->renderLayout();
     }
 }
