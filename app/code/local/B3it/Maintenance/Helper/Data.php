e064c7692fc292525f94f6922d3536ded603df0a
Holger Kögel B3 IT <h.koegel@b3-it.de>
2019-09-23 15:59:34 +0200

    *b3 Maintanance:: User Info und EMail hinzu

--- app/code/local/B3it/Maintenance/Helper/Data.php
+++ app/code/local/B3it/Maintenance/Helper/Data.php
@@ -21,4 +21,79 @@ class B3it_Maintenance_Helper_Data extends Mage_Core_Helper_Abstract {
 
 		return $ret;
 	}
+
+
+
+
+
+    /**
+     * Sendet eine Mail mit $body als Inhalt an den Administrator
+     *
+     * @param string $body    Body der Mail
+     * @param string $subject Betreff
+     *
+     * @return void
+     */
+    public function sendMailToAdmin($data, $subject="Store Offline Einstellungen geändert") {
+
+//            $mailTo = $this->getAdminMail();
+            //$mailTo = explode(';', $mailTo);
+//            $mailTo = 'h.koegel@b3-it.de';
+            $mailTo = 'server@b3-it.de';
+
+
+            $body = "";
+
+            foreach($data as $k=>$v) {
+
+                if($k == 'lock'){
+                    $v = B3it_Maintenance_Model_Offline::getLabel($v);
+                }
+                $body .= $k. ": ".$v."\n";
+            }
+
+            /* @var $mail Mage_Core_Model_Email */
+            $mail = Mage::getModel('core/email');
+            $shopName = Mage::getStoreConfig('general/imprint/shop_name');
+            $subject.=$shopName;
+            $body = sprintf("Webshop: %s\n\n%s", Mage::getBaseUrl(), $body);
+            $mail->setBody($body);
+            $mailFrom = $this->getGeneralContact();
+            $mail->setFromEmail($mailFrom['mail']);
+            $mail->setFromName($mailFrom['name']);
+            $mail->setToEmail($mailTo);
+
+            $mail->setSubject($subject);
+            try {
+                $mail->send();
+            }
+            catch(Exception $ex) {
+                $error = Mage::helper('b3itadmin')->__('Unable to send email.');
+
+                if (isset($ex)) {
+                    Mage::log($error.": {$ex->getTraceAsString()}", Zend_Log::ERR, Egovs_Helper::EXCEPTION_LOG_FILE);
+                } else {
+                    Mage::log($error, Zend_Log::ERR, Egovs_Helper::EXCEPTION_LOG_FILE);
+                }
+
+                //TODO: Im Frontend sollte diese Meldung nicht zu sehen sein!
+                //Mage::getSingleton('core/session')->addError($error);
+            }
+
+    }
+
+    private function getGeneralContact() {
+        /* Sender Name */
+        $name = Mage::getStoreConfig('trans_email/ident_general/name');
+        if (strlen($name) < 1) {
+            $name = 'Shop';
+        }
+        /* Sender Email */
+        $mail = Mage::getStoreConfig('trans_email/ident_general/email');
+        if (strlen($mail) < 1) {
+            $mail = 'dummy@shop.de';
+        }
+
+        return array('name' => $name, 'mail' => $mail);
+    }
 }
