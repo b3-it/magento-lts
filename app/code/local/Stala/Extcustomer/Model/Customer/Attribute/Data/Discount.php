aee4e47827c39f17c9f27d3c42848e9485b3790d
René Mütterlein <r.muetterlein@b3-it.de>
2019-12-06 12:32:00 +0100

    *BugFi :: Warning: count(): Parameter must be an array or an object that implements Countable

--- app/code/local/Stala/Extcustomer/Model/Customer/Attribute/Data/Discount.php
+++ app/code/local/Stala/Extcustomer/Model/Customer/Attribute/Data/Discount.php
@@ -13,11 +13,11 @@ class Stala_Extcustomer_Model_Customer_Attribute_Data_Discount extends Mage_Eav_
 {
 	/**
 	 * Validiert die Daten
-	 * 
+	 *
 	 * Gibt true oder ein Array mit Fehlern zurück.
 	 *
 	 * @param array|string $value Value
-	 * 
+	 *
 	 * @return boolean|array
 	 */
 	public function validateValue($value)
@@ -25,23 +25,23 @@ class Stala_Extcustomer_Model_Customer_Attribute_Data_Discount extends Mage_Eav_
 		$errors     = array();
 		$attribute  = $this->getAttribute();
 		$label      = Mage::helper('eav')->__($attribute->getStoreLabel());
-	
+
 		if ($value === false) {
 			// try to load original value and validate it
 			$value = $this->getEntity()->getDataUsingMethod($attribute->getAttributeCode());
 		}
-	
-		if ($attribute->getIsRequired() && (!isset($value) || null == $value || count($value) < 1)) {
+
+		if ($attribute->getIsRequired() && (!isset($value) || null == $value || (is_array($value) &&  count($value) < 1) )) {
 			$errors[] = Mage::helper('eav')->__('"%s" is a required value.', $label);
 		}
-	
+
 		if (!$errors && !$attribute->getIsRequired() && empty($value)) {
 			return true;
 		}
-	
+
 		// validate length
 		$length = Mage::helper('core/string')->strlen(trim($value));
-	
+
 		$validateRules = $attribute->getValidateRules();
 		if (!empty($validateRules['min_text_length']) && $length < $validateRules['min_text_length']) {
 			$v = $validateRules['min_text_length'];
@@ -51,7 +51,7 @@ class Stala_Extcustomer_Model_Customer_Attribute_Data_Discount extends Mage_Eav_
 			$v = $validateRules['max_text_length'];
 			$errors[] = Mage::helper('eav')->__('"%s" length must be equal or less than %s characters.', $label, $v);
 		}
-	
+
 		$result = $this->_validateInputRule($value);
 		if ($result !== true) {
 			$errors = array_merge($errors, $result);
@@ -59,7 +59,7 @@ class Stala_Extcustomer_Model_Customer_Attribute_Data_Discount extends Mage_Eav_
 		if (count($errors) == 0) {
 			return true;
 		}
-	
+
 		return $errors;
 	}
 }
